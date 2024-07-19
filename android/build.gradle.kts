8ea1bddb156a898da34c5a1c07939573b8d05b53
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-07-19 14:48:05 +0900

    chore: jetpack navigation 라이브러리 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val navigationVersion = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
