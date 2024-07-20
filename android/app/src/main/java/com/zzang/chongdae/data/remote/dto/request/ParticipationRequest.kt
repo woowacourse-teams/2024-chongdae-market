5dea2dd6cff058d00ddd998cbb87ad6587f0e4f5
Namyunsuk <yunsuk0328@naver.com>
2024-07-21 02:14:11 +0900

    refactor: request Dto에 SerialName적용

--- android/app/src/main/java/com/zzang/chongdae/data/remote/dto/request/ParticipationRequest.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/dto/request/ParticipationRequest.kt
@@ -1,8 +1,9 @@
 package com.zzang.chongdae.data.remote.dto.request
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-class ParticipationRequest(
-    val articleId: Long,
+data class ParticipationRequest(
+    @SerialName("articleId") val articleId: Long,
 )
