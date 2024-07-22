ae8369160ac740216a26d82d433147412e107fdf
fromitive <46563149+fromitive@users.noreply.github.com>
2024-07-22 18:07:04 +0900

    feat: 도메인 추가 (#15)
    * feat: BaseTimeEntity 추가
    
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: Member Entity 추가
    
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: Offering Entity 추가
    
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: OfferingMember Entity 추가
    
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: Comment Entity 추가
    
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    ---------
    
    Co-authored-by: Dora Choo <choo000407@naver.com>

---
+++ backend/src/main/java/com/zzang/chongdae/common/repository/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.zzang.chongdae.common.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @NotNull
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @NotNull
+    @Column
+    private LocalDateTime updatedAt;
+}
