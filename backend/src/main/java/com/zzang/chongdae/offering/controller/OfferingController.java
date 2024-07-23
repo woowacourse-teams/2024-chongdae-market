61630dc160c3ebcc865444d0e94428939854d47f
fromitive <fromitive@gmail.com>
2024-07-23 14:21:20 +0900

    feat: 공모 상세 조회 API 문서화
    Co-authored-by: Dora Choo <choo000407@naver.com>

--- backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingController.java
+++ backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingController.java
@@ -2,18 +2,22 @@ package com.zzang.chongdae.offering.controller;
 
 import com.zzang.chongdae.offering.service.OfferingService;
 import com.zzang.chongdae.offering.service.dto.OfferingDetailResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name = "Offerring(공모)")
 @RequiredArgsConstructor
 @Controller
 public class OfferingController {
 
     private final OfferingService offeringService;
 
+    @Operation(summary = "공모 상세 조회", description = "공모 id를 통해 공모의 상세 정보를 조회합니다.")
     @GetMapping("/offerings/{offering-id}")
     public ResponseEntity<OfferingDetailResponse> getOfferingDetail(@PathVariable(value = "offering-id") Long id) {
         OfferingDetailResponse response = offeringService.getOfferingDetail(id);
