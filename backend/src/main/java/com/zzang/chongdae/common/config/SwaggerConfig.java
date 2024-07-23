ac7b9cec114cf52e04bffb4fc3d3959ca5ed22a2
fromitive <fromitive@gmail.com>
2024-07-23 14:20:57 +0900

    feat: SwaggerConfig 파일 추가
    Co-authored-by: Dora Choo <choo000407@naver.com>

---
+++ backend/src/main/java/com/zzang/chongdae/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.zzang.chongdae.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("총대마켓 API 명세서")
+                .description("총대마켓 백엔드 API 명세서")
+                .version("0.1.0");
+    }
+}
