d305232e4a7e460c25101ff408e1498d87d56eb9
SCY <helenason@naver.com>
2024-07-24 17:57:34 +0900

    feat: 댓글 목록 조회 API 구현 (#66)

--- backend/src/main/java/com/zzang/chongdae/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/zzang/chongdae/comment/repository/CommentRepository.java
@@ -1,7 +1,21 @@
 package com.zzang.chongdae.comment.repository;
 
+import com.zzang.chongdae.comment.domain.CommentWithRole;
 import com.zzang.chongdae.comment.repository.entity.CommentEntity;
+import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    @Query("""
+            SELECT new com.zzang.chongdae.comment.domain.CommentWithRole(
+                c, om.role
+            )
+            FROM CommentEntity as c JOIN OfferingMemberEntity as om
+                ON c.offering = om.offering AND c.member = om.member
+            WHERE om.offering = :offering
+        """)
+    List<CommentWithRole> findAllWithRoleByOffering(OfferingEntity offering);
 }
