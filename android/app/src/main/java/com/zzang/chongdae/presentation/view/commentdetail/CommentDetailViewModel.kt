2b8195d4c24b23034a630e0ca43b140fbd943a7d
채현 <wjdcogus202@sookmyung.ac.kr>
2024-07-24 20:33:42 +0900

    feat 댓글방 접히는 공지 뷰 구현 (#72)
    * chore: manifest에 CommentDetailActivity 추가
    
    * feat: BindingAdatper을 사용하여 접힐 때 애니메이션 적용 및 픽셀 변환
    
    * feat: viewmodel 구현 및 click 마다 접히고 펴지는 로직 구현
    
    * style: ktlint 적용
    
    * refactor: binding adpater을 사용하여 가시성 변경

---
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModel.kt
@@ -0,0 +1,34 @@
+package com.zzang.chongdae.presentation.view.commentdetail
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+
+class CommentDetailViewModel(
+    private val offeringId: Long,
+    private val offeringTitle: String,
+) : ViewModel() {
+    private val _isCollapsibleViewVisible = MutableLiveData<Boolean>(false)
+    val isCollapsibleViewVisible: LiveData<Boolean> get() = _isCollapsibleViewVisible
+
+    fun toggleCollapsibleView() {
+        _isCollapsibleViewVisible.value = _isCollapsibleViewVisible.value?.not()
+        Log.d("CommentDetailViewModel", "toggleCollapsibleView: ${_isCollapsibleViewVisible.value}")
+    }
+
+    companion object {
+        @Suppress("UNCHECKED_CAST")
+        fun getFactory(
+            offeringId: Long,
+            offeringTitle: String,
+        ) = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras,
+            ): T = CommentDetailViewModel(offeringId, offeringTitle) as T
+        }
+    }
+}
