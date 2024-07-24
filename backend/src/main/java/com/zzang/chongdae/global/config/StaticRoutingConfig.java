565ea232064fe3fee556af15467ca63da21c0294
Choo <choo000407@naver.com>
2024-07-24 16:56:25 +0900

    chore: swagger ui 정적 파일 설치 및 static routing 세팅

---
+++ backend/src/main/java/com/zzang/chongdae/global/config/StaticRoutingConfig.java
@@ -0,0 +1,17 @@
+package com.zzang.chongdae.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticRoutingConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/static/swagger-ui.html");
+    }
+}
