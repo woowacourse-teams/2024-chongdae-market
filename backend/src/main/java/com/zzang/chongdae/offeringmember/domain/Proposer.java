8402b405a78ed23c3dffcefdcdec3430223a561c
fromitive <fromitive@gmail.com>
2024-07-25 13:15:33 +0900

    docs: 테스트 데이터 변경

---
+++ backend/src/main/java/com/zzang/chongdae/offeringmember/domain/Proposer.java
@@ -0,0 +1,4 @@
+package com.zzang.chongdae.offeringmember.domain;
+
+public class Proposer {
+}
--- backend/src/test/java/com/zzang/chongdae/ChongdaeApplicationTests.java
+++ backend/src/test/java/com/zzang/chongdae/ChongdaeApplicationTests.java
@@ -1,13 +1,15 @@
 package com.zzang.chongdae;
 
+import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChongdaeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        RestAssuredRestDocumentationWrapper.document();
+    }
 
 }
