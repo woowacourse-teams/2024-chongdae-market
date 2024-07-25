b93ed9d8f3ab3704743cbd6cbc009722266e388d
채현 <wjdcogus202@sookmyung.ac.kr>
2024-07-25 12:33:29 +0900

    feat: 댓글방 접히는 공지 API 연결 (#85)
    * feat: 미팅 일정 API 연결을 위한 data layer 구현
    
    * feat: 미팅 일정 API 연결을 위한 domain layer 구현
    
    * feat: 미팅 일정 API 연결을 위한 presentation layer 구현
    
    * style: ktlint 적용
    
    * feat: 공동 구매 제목 databinding 적용
    
    * refactor: 변수명 수정
    
    * fix: 펼치기 접기 버튼 로직 반대로 수정
    
    * style: ktlint 적용

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModel.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModel.kt
@@ -5,18 +5,48 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.zzang.chongdae.domain.repository.CommentDetailRepository
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class CommentDetailViewModel(
     private val offeringId: Long,
-    private val offeringTitle: String,
+    val offeringTitle: String,
+    private val commentDetailRepository: CommentDetailRepository,
 ) : ViewModel() {
     private val _isCollapsibleViewVisible = MutableLiveData<Boolean>(false)
     val isCollapsibleViewVisible: LiveData<Boolean> get() = _isCollapsibleViewVisible
 
+    private val _deadline = MutableLiveData<LocalDateTime>()
+    val deadline: LiveData<LocalDateTime> get() = _deadline
+
+    private val _location = MutableLiveData<String>()
+    val location: LiveData<String> get() = _location
+
+    private val _locationDetail = MutableLiveData<String>()
+    val locationDetail: LiveData<String> get() = _locationDetail
+
     fun toggleCollapsibleView() {
         _isCollapsibleViewVisible.value = _isCollapsibleViewVisible.value?.not()
-        Log.d("CommentDetailViewModel", "toggleCollapsibleView: ${_isCollapsibleViewVisible.value}")
+        if (_isCollapsibleViewVisible.value == true) {
+            loadMeetings()
+        }
+    }
+
+    private fun loadMeetings() {
+        viewModelScope.launch {
+            commentDetailRepository.getMeetings(
+                offeringId = offeringId,
+            ).onSuccess {
+                _deadline.value = it.deadline
+                _location.value = it.meetingAddress
+                _locationDetail.value = it.meetingAddressDetail
+            }.onFailure {
+                Log.e("error", it.message.toString())
+            }
+        }
     }
 
     companion object {
@@ -24,11 +54,12 @@ class CommentDetailViewModel(
         fun getFactory(
             offeringId: Long,
             offeringTitle: String,
+            commentDetailRepository: CommentDetailRepository,
         ) = object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(
                 modelClass: Class<T>,
                 extras: CreationExtras,
-            ): T = CommentDetailViewModel(offeringId, offeringTitle) as T
+            ): T = CommentDetailViewModel(offeringId, offeringTitle, commentDetailRepository) as T
         }
     }
 }
