1d5d77182940d4aebe382f1dd7ece539e5cf4b68
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-07-26 02:05:42 +0900

    feat: 공동구매 상태를 관리하는 enum class 구현

---
+++ android/app/src/main/java/com/zzang/chongdae/domain/model/OfferingStatus.kt
@@ -0,0 +1,22 @@
+package com.zzang.chongdae.domain.model
+
+import androidx.annotation.DrawableRes
+import com.zzang.chongdae.R
+
+enum class OfferingStatus(
+    val statusText: String,
+    val buttonText: String,
+    @DrawableRes val imageRes: Int,
+) {
+    Recruiting("인원확정", "일정수정", R.drawable.ic_comment_detail_recruiting),
+    Scheduling("일정확정", "일정수정", R.drawable.ic_comment_detail_scheduling),
+    Purchasing("구매확정", "일정수정", R.drawable.ic_comment_detail_purchasing),
+    Dealing("거래확정", "신고하기", R.drawable.ic_comment_detail_dealing),
+    Completed("거래완료", "신고하기", R.drawable.ic_comment_detail_completed);
+    
+    companion object {
+        fun nextStatus(currentStatus: OfferingStatus): OfferingStatus {
+            return entries[(currentStatus.ordinal + 1) % entries.size]
+        }
+    }
+}
