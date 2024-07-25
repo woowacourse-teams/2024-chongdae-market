e31350a7e10ebfe5d3930348b82940674f2954c3
SCY <helenason@naver.com>
2024-07-25 14:01:56 +0900

    refactor: 댓글 도메인 코드 리팩터링 (#96)
    * refactor: 로그인 멤버 변수명 변경
    
    * refactor: JPQL 쿼리 컨벤션 및 멤버로 공모 조회 메서드명 변경
    
    * refactor: 최근 댓글 응답 클래스명 변경
    
    * refactor: 컨트롤러 및 서비스 API 순서 변경
    
    * refactor: 로그인 사용자 유효성 검증

--- backend/src/main/java/com/zzang/chongdae/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/zzang/chongdae/comment/repository/CommentRepository.java
@@ -11,14 +11,12 @@ import org.springframework.data.jpa.repository.Query;
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
     @Query("""
-            SELECT new com.zzang.chongdae.comment.domain.CommentWithRole(
-                c, om.role
-            )
-            FROM CommentEntity as c JOIN OfferingMemberEntity as om
-                ON c.offering = om.offering AND c.member = om.member
-            WHERE om.offering = :offering
-            ORDER BY c.createdAt
-        """)
+                SELECT new com.zzang.chongdae.comment.domain.CommentWithRole(c, om.role)
+                FROM CommentEntity as c JOIN OfferingMemberEntity as om
+                    ON c.offering = om.offering AND c.member = om.member
+                WHERE om.offering = :offering
+                ORDER BY c.createdAt
+            """)
     List<CommentWithRole> findAllWithRoleByOffering(OfferingEntity offering);
 
     Optional<CommentEntity> findTopByOfferingOrderByCreatedAtDesc(OfferingEntity offering);
