381126f3bfe075696e8dc7a9ed5f611fcc2a3678
alsong <138569524+songpink@users.noreply.github.com>
2024-07-25 11:24:22 +0900

    feat: 댓글방 목록 API 연결 (#82)
    * feat: bottom navigation fragment 추가
    
    * feat: vector 이미지 추가
    
    * feat: 댓글방이 없으면 "채팅 목록이 없어요" 라는 텍스트뷰와 이미지뷰를 띄우는 기능 구현
    
    * feat: 댓글방 띄우는 기능 구현
    
    * test: 댓글방 UI 테스트 작성
    
    * refactor: 테스트 클래스명 수정
    
    * refactor: 줄바꿈 수정
    
    * feat: 댓글방 API 서비스 구현
    
    * refactor: API 명세에 따라 도메인 모델 수정
    
    * feat: API 연결
    
    * refactor: API명세에 따라 데이터바인딩 변수명 수정
    
    * feat: 댓글방 목록 API 연결
    
    * refactor: ktlint Format 적용
    
    * refactor: 메모리 누수 방지를 위해 fragment가 destroy 될 때 _binding을 null로 설정
    
    * refactor: 어답터를 방어적복사 하지 않아도 되어서 수정
    
    * refactor: 채팅방이 없다는 이미지뷰를 띄워주는 방식 수정(바인딩 어댑터 수정)
    
    * refactor: 함수분리
    
    * refactor: ktFormat 적용
    
    ---------
    
    Co-authored-by: chaehyun <80222352+chaehyuns@users.noreply.github.com>

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/comment/CommentRoomFragment.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/comment/CommentRoomFragment.kt
@@ -5,14 +5,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.zzang.chongdae.R
+import androidx.fragment.app.viewModels
+import com.zzang.chongdae.data.remote.api.NetworkManager
+import com.zzang.chongdae.data.remote.source.impl.CommentRoomDataSourceImpl
+import com.zzang.chongdae.data.repository.remote.CommentRoomRepositoryImpl
+import com.zzang.chongdae.databinding.FragmentCommentRoomBinding
+import com.zzang.chongdae.presentation.view.comment.adapter.CommentRoomAdapter
 
 class CommentRoomFragment : Fragment() {
+    private var _binding: FragmentCommentRoomBinding? = null
+    private val binding get() = _binding!!
+
+    private val commentRoomAdapter: CommentRoomAdapter by lazy {
+        CommentRoomAdapter(viewModel)
+    }
+
+    val viewModel by viewModels<CommentRoomViewModel> {
+        CommentRoomViewModel.factory(
+            CommentRoomRepositoryImpl(
+                CommentRoomDataSourceImpl(NetworkManager.commentRoomService()),
+            ),
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_comment_room, container, false)
+    ): View {
+        initBinding(inflater, container)
+        linkAdapter()
+        updateCommentRooms()
+
+        return binding.root
+    }
+
+    private fun initBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ) {
+        _binding = FragmentCommentRoomBinding.inflate(inflater, container, false)
+        binding.fragmentCommentRoom = this
+        binding.vm = viewModel
+    }
+
+    private fun linkAdapter() {
+        binding.rvCommentRoom.adapter = commentRoomAdapter
+        viewModel.commentRooms.observe(viewLifecycleOwner) {
+            commentRoomAdapter.submitList(it)
+        }
+        commentRoomAdapter.submitList(viewModel.commentRooms.value)
+    }
+
+    private fun updateCommentRooms() {
+        viewModel.updateCommentRooms()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
