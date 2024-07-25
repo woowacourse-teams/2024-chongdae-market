57bf1faedd9632dba54d0cecbbe59d075461f378
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-07-25 13:46:38 +0900

    feat: post comment api service 구현

--- android/app/src/main/java/com/zzang/chongdae/data/remote/api/CommentDetailApiService.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/api/CommentDetailApiService.kt
@@ -1,8 +1,11 @@
 package com.zzang.chongdae.data.remote.api
 
+import com.zzang.chongdae.data.remote.dto.request.CommentRequest
 import com.zzang.chongdae.data.remote.dto.response.MeetingsResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface CommentDetailApiService {
@@ -10,4 +13,9 @@ interface CommentDetailApiService {
     suspend fun getMeetings(
         @Path("offering-id") offeringId: Long,
     ): Response<MeetingsResponse>
+
+    @POST("/comments")
+    suspend fun postComment(
+        @Body commentRequest: CommentRequest,
+    ): Response<Unit>
 }
