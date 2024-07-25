06bced87d67ccd9146d2270325b67be9df571f75
songpink <jejeje04@naver.com>
2024-07-25 23:38:03 +0900

    fix: 마지막 댓글 response를 nullable하게 수정

--- android/app/src/main/java/com/zzang/chongdae/data/mapper/CommentRoomResponseMapper.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/mapper/CommentRoomResponseMapper.kt
@@ -2,13 +2,14 @@ package com.zzang.chongdae.data.mapper
 
 import com.zzang.chongdae.data.remote.dto.response.CommentRoomResponse
 import com.zzang.chongdae.domain.model.CommentRoom
+import java.time.LocalDateTime
 
 fun CommentRoomResponse.toDomain(): CommentRoom {
     return CommentRoom(
         id = this.offeringId,
         title = this.offeringTitle,
-        latestComment = this.latestComment.content,
-        latestCommentTime = this.latestComment.createdAt.toLocalDateTime(),
+        latestComment = this.latestComment.content ?: "",
+        latestCommentTime = this.latestComment.createdAt?.toLocalDateTime(),
         isProposer = this.isProposer,
     )
 }
