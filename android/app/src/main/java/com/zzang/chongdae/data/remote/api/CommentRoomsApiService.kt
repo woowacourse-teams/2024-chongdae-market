40804ba0e92c72f165c18ef45f074ed42ca44359
alsong <138569524+songpink@users.noreply.github.com>
2024-07-29 20:04:04 +0900

    refactor: 네이밍 수정 (#123)

--- android/app/src/main/java/com/zzang/chongdae/data/remote/api/CommentRoomsApiService.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/api/CommentRoomsApiService.kt
@@ -1,13 +1,13 @@
 package com.zzang.chongdae.data.remote.api
 
-import com.zzang.chongdae.data.remote.dto.response.CommentRoomListResponse
+import com.zzang.chongdae.data.remote.dto.response.CommentRoomsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CommentRoomApiService {
+interface CommentRoomsApiService {
     @GET("/comments")
-    suspend fun getCommentRoom(
+    suspend fun getCommentRooms(
         @Query("member-id") memberId: Long,
-    ): Response<CommentRoomListResponse>
+    ): Response<CommentRoomsResponse>
 }
--- android/app/src/main/java/com/zzang/chongdae/data/remote/source/CommentRoomDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.zzang.chongdae.data.remote.source
-
-import com.zzang.chongdae.data.remote.dto.response.CommentRoomListResponse
-
-interface CommentRoomDataSource {
-    suspend fun fetchCommentRooms(memberId: Long): Result<CommentRoomListResponse>
-}
