c51752a15833966dd2925b31c12249316814120d
Choo <choo000407@naver.com>
2024-08-01 16:23:35 +0900

    fix: 사진 url 속에 버킷이름을 cloudfront 도메인으로 수정

--- backend/src/main/java/com/zzang/chongdae/storage/service/StorageService.java
+++ backend/src/main/java/com/zzang/chongdae/storage/service/StorageService.java
@@ -4,7 +4,6 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.zzang.chongdae.global.exception.MarketException;
 import com.zzang.chongdae.storage.exception.StorageErrorCode;
 import java.io.IOException;
@@ -14,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Service
@@ -24,15 +24,17 @@ public class StorageService {
     @Value("${amazon.s3.bucket}")
     private String bucketName;
 
+    @Value("${amazon.cloudfront.redirectUrl}")
+    private String redirectUrl;
+
     public String uploadFile(MultipartFile file, String path) {
         try {
             String objectKey = path + UUID.randomUUID();
-            ObjectMetadata metadata = createMetadata(file);
             InputStream inputStream = file.getInputStream();
+            ObjectMetadata metadata = createMetadata(file);
             PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectKey, inputStream, metadata);
-
             s3Client.putObject(putObjectRequest);
-            return s3Client.getUrl(bucketName, objectKey).toString();
+            return createUri(objectKey);
         } catch (IOException e) {
             throw new MarketException(StorageErrorCode.INVALID_FILE);
         } catch (SdkClientException e) {
@@ -45,5 +47,14 @@ public class StorageService {
         metadata.setContentLength(file.getSize());
         metadata.setContentType(file.getContentType());
         return metadata;
+    }
+
+    private String createUri(String objectKey) {
+        return UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host(redirectUrl)
+                .path("/" + objectKey)
+                .build(false)
+                .toString();
     }
 }
