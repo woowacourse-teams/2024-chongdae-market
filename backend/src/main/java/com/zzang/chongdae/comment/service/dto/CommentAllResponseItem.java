9cfe4d1a2ca297a4b1b14d04e942eb827bc1e3ed
SCY <helenason@naver.com>
2024-08-01 16:31:02 +0900

    feat: 댓글방 메시지 조회 시 commentId 필드 추가 (#150)
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>

--- backend/src/main/java/com/zzang/chongdae/comment/service/dto/CommentAllResponseItem.java
+++ backend/src/main/java/com/zzang/chongdae/comment/service/dto/CommentAllResponseItem.java
@@ -1,6 +1,7 @@
 package com.zzang.chongdae.comment.service.dto;
 
-public record CommentAllResponseItem(CommentCreatedAtResponse createdAt,
+public record CommentAllResponseItem(Long commentId,
+                                     CommentCreatedAtResponse createdAt,
                                      String content,
                                      String nickname,
                                      Boolean isProposer,
