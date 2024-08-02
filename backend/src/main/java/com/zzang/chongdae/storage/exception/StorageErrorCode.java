bb4d1da58dd3a92e851ceddf8808d7a7c188e67b
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2024-08-02 16:25:33 +0900

    feat: s3 이미지 업로드 API 구현 (#147)
    * feat: s3 이미지 업로드 API 구현
    
    * chore: cicd 액션 범위 수정
    
    * fix: 이미지 업로드 경로의 특수문자 제거
    
    * chore: yml multipart 설정 추가
    
    * chore: S3 업로드 결과 테스트
    
    * fix: inputstream 변환로직 위치 이동
    
    * fix: 업로드할 s3 path 올바르게 수정
    
    * fix: 사진 url 속에 버킷이름을 cloudfront 도메인으로 수정
    
    * chore: actions 범위 재조정
    
    * feat: API endpoint 변경
    
    * chore: docker image 지우는 작업을 마지막으로 이동
    
    * chore: 다른 브랜치로 이전 커밋 이동하기 위해 제거
    
    * chore: 충돌 해결 및 코드 스타일 변경
    
    * test: S3 이미지 업로드 성공 케이스 추가
    
    * test: multipart form data 문서화
    
    * test: 공모 상태 enum 문서화
    
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>
    
    * feat: 파일 업로드 크기 제한 100MB에서 20MB로 변경
    
    ---------
    
    Co-authored-by: Choo <choo000407@naver.com>
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>

--- backend/src/main/java/com/zzang/chongdae/storage/exception/StorageErrorCode.java
+++ backend/src/main/java/com/zzang/chongdae/storage/exception/StorageErrorCode.java
@@ -1,6 +1,7 @@
-package com.zzang.chongdae.offering.exception;
+package com.zzang.chongdae.storage.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import com.zzang.chongdae.global.exception.ErrorMessage;
 import com.zzang.chongdae.global.exception.ErrorResponse;
@@ -10,11 +11,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum OfferingErrorCode implements ErrorResponse {
+public enum StorageErrorCode implements ErrorResponse {
 
-    NOT_FOUND(BAD_REQUEST, "해당 공모가 존재하지 않습니다."),
-    PARTICIPANT_FULL(BAD_REQUEST, "해당 공모에 참여 가능한 인원수를 초과하였습니다."),
-    CANNOT_PARTICIPATE(BAD_REQUEST, "참여할 수 없는 공모입니다.");
+    INVALID_FILE(BAD_REQUEST, "유효한 파일이 아닙니다."),
+    STORAGE_SERVER_FAIL(INTERNAL_SERVER_ERROR, "이미지 서버에 문제가 발생했습니다.");
 
     private final HttpStatus status;
     private final String message;
