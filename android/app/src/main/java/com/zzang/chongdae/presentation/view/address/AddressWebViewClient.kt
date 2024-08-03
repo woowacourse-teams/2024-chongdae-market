b1de3e4bd323aa3c5fa8f77914432a7b2123e36c
Namyunsuk <yunsuk0328@naver.com>
2024-08-03 18:45:11 +0900

    feat: 주소검색 기능 구현

---
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/address/AddressWebViewClient.kt
@@ -0,0 +1,17 @@
+package com.zzang.chongdae.presentation.view.address
+
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
+
+class AddressWebViewClient(private val assetLoader: WebViewAssetLoader) :
+    WebViewClientCompat() {
+    override fun shouldInterceptRequest(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): WebResourceResponse? {
+        return assetLoader.shouldInterceptRequest(request!!.url)
+    }
+}
