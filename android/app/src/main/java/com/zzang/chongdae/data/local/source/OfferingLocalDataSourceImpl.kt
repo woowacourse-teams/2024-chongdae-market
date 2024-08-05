6484271029378e83eba12831f43f89b9b86158ec
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-05 17:58:58 +0900

    feat: LocalDataSourceImpl 구현

---
+++ android/app/src/main/java/com/zzang/chongdae/data/local/source/OfferingLocalDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.zzang.chongdae.data.local.source
+
+import com.zzang.chongdae.data.local.dao.OfferingDao
+import com.zzang.chongdae.data.local.model.OfferingEntity
+import com.zzang.chongdae.data.source.offering.OfferingLocalDataSource
+
+class OfferingLocalDataSourceImpl(private val offeringDao: OfferingDao): OfferingLocalDataSource {
+   override suspend fun insertOfferings(offerings: List<OfferingEntity>) {
+        offeringDao.insertAll(offerings)
+    }
+    
+    override suspend fun insertOffering(offering: OfferingEntity) {
+        offeringDao.insertOffering(offering)
+    }
+}
+
+
