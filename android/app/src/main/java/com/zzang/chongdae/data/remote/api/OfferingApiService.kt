1bd21b3dca4da5628798cac37ce47db3b1df0e31
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-06 11:19:42 +0900

    feat: api service 구현

--- android/app/src/main/java/com/zzang/chongdae/data/remote/api/OfferingApiService.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/api/OfferingApiService.kt
@@ -1,9 +1,11 @@
 package com.zzang.chongdae.data.remote.api
 
 import com.zzang.chongdae.data.remote.dto.request.OfferingWriteRequest
+import com.zzang.chongdae.data.remote.dto.request.ProductUrlRequest
 import com.zzang.chongdae.data.remote.dto.response.MeetingsResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingDetailResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingsResponse
+import com.zzang.chongdae.data.remote.dto.response.ProductUrlResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -33,4 +35,9 @@ interface OfferingApiService {
     suspend fun postOfferingWrite(
         @Body offeringWriteRequest: OfferingWriteRequest,
     ): Response<Unit>
+    
+    @POST("/offerings/product-images/og")
+    suspend fun postProductImageOg(
+        @Body productUrl: ProductUrlRequest,
+    ): Response<ProductUrlResponse>
 }
