f83cedc5a4445b725e1748af4d08a446a47b4de0
fromitive <fromitive@gmail.com>
2024-08-06 19:44:51 +0900

    fix: nicknameWordInitializer 설정 오류 해결

--- backend/src/main/java/com/zzang/chongdae/member/service/NicknameWordReader.java
+++ backend/src/main/java/com/zzang/chongdae/member/service/NicknameWordReader.java
@@ -3,19 +3,19 @@ package com.zzang.chongdae.member.service;
 import com.zzang.chongdae.global.exception.MarketException;
 import com.zzang.chongdae.member.exception.MemberErrorCode;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NicknameWordReader {
 
     public List<String> read(String path) {
-        try (FileInputStream fileInputStream = new FileInputStream(path);
-             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        ClassPathResource resource = new ClassPathResource(path);
+        try (InputStreamReader inputStreamReader = new InputStreamReader(resource.getInputStream());
              BufferedReader br = new BufferedReader(inputStreamReader)) {
             String[] line = br.readLine().split(",");
             return Arrays.asList(line);
