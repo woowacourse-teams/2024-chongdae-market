a2fdecb2ab58c6f485bc17eaf97ca5f33fa6b05f
fromitive <fromitive@gmail.com>
2024-08-06 16:17:09 +0900

    refactor: ci값이 일치하지 않을경우 오류메시지 문구 변경

--- backend/src/main/java/com/zzang/chongdae/auth/exception/AuthErrorCode.java
+++ backend/src/main/java/com/zzang/chongdae/auth/exception/AuthErrorCode.java
@@ -12,7 +12,7 @@ public enum AuthErrorCode implements ErrorResponse {
 
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "가입되지 않은 회원입니다."),
+    INVALID_PASSWORD(HttpStatus.NOT_FOUND, "가입하지 않은 회원입니다."),
     DUPLICATED_MEMBER(HttpStatus.CONFLICT, "이미 가입한 회원입니다.");
 
     private final HttpStatus status;
