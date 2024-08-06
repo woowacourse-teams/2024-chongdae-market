0de8f080188f0211d3241a34cd0fd8cb5cef80bb
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-06 11:20:03 +0900

    feat: datasource 구현

--- android/app/src/main/java/com/zzang/chongdae/data/remote/source/OfferingRemoteDataSourceImpl.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/source/OfferingRemoteDataSourceImpl.kt
@@ -1,9 +1,12 @@
 package com.zzang.chongdae.data.remote.source
 
+import com.zzang.chongdae.data.mapper.toProductUrlRequest
 import com.zzang.chongdae.data.remote.api.OfferingApiService
 import com.zzang.chongdae.data.remote.dto.request.OfferingWriteRequest
 import com.zzang.chongdae.data.remote.dto.response.OfferingsResponse
+import com.zzang.chongdae.data.remote.dto.response.ProductUrlResponse
 import com.zzang.chongdae.data.source.offering.OfferingRemoteDataSource
+import retrofit2.Response
 
 class OfferingRemoteDataSourceImpl(
     private val service: OfferingApiService,
@@ -15,10 +18,23 @@ class OfferingRemoteDataSourceImpl(
         runCatching {
             service.getOfferings(lastOfferingId, pageSize).body() ?: throw IllegalStateException()
         }
-
+    
     override suspend fun saveOffering(offeringWriteRequest: OfferingWriteRequest): Result<Unit> {
         return runCatching {
             val response = service.postOfferingWrite(offeringWriteRequest)
+            if (response.isSuccessful) {
+                response.body() ?: error("에러 발생: null")
+            } else {
+                error("에러 발생: ${response.code()}")
+            }
+        }
+    }
+    
+    override suspend fun saveProductImageOg(
+        productUrl: String,
+    ): Result<ProductUrlResponse> {
+        return runCatching {
+            val response: Response<ProductUrlResponse> = service.postProductImageOg(productUrl.toProductUrlRequest())
             if (response.isSuccessful) {
                 response.body() ?: error("에러 발생: null")
             } else {
--- android/app/src/main/java/com/zzang/chongdae/data/source/offering/OfferingRemoteDataSource.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/source/offering/OfferingRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.zzang.chongdae.data.source.offering
 
 import com.zzang.chongdae.data.remote.dto.request.OfferingWriteRequest
+import com.zzang.chongdae.data.remote.dto.response.OfferingDetailResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingsResponse
+import com.zzang.chongdae.data.remote.dto.response.ProductUrlResponse
 
 interface OfferingRemoteDataSource {
     suspend fun fetchOfferings(
@@ -10,4 +12,8 @@ interface OfferingRemoteDataSource {
     ): Result<OfferingsResponse>
 
     suspend fun saveOffering(offeringWriteRequest: OfferingWriteRequest): Result<Unit>
+    
+    suspend fun saveProductImageOg(
+        productUrl: String,
+    ): Result<ProductUrlResponse>
 }
