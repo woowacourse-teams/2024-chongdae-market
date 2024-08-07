f8ad9a12a4277901b2d998e88774a3c46f1cf186
Dora Choo <choo000407@naver.com>
2024-08-07 19:08:22 +0900

    feat: 토큰 재발급 API에서 requestHeader로 refreshToken 받도록 수정 (#227)
    * feat: 토큰 재발급 API에서 body가 아닌 cookie로 토큰 반환
    
    * feat: 회원가입 API도 body가 아닌 cookie로 토큰 반환
    
    * refactor: service 용 dto 명 컨벤션에 맞춰 수정

--- backend/src/main/java/com/zzang/chongdae/auth/service/dto/SignupResponse.java
+++ backend/src/main/java/com/zzang/chongdae/auth/service/dto/SignupResponse.java
@@ -1,10 +1,8 @@
 package com.zzang.chongdae.auth.service.dto;
 
-import com.zzang.chongdae.member.repository.entity.MemberEntity;
+public record SignupResponse(Long memberId, String nickname) {
 
-public record SignupResponse(SignupMemberResponseItem member, SignupTokenResponseItem token) {
-
-    public SignupResponse(MemberEntity savedMember, TokenDto tokenDto) {
-        this(new SignupMemberResponseItem(savedMember), new SignupTokenResponseItem(tokenDto));
+    public SignupResponse(SignupResponseDto output) {
+        this(output.member().id(), output.member().nickname());
     }
 }
