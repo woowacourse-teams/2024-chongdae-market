b7f2b9867296ec02f6ffa88ce29755044e9ee718
Choo <choo000407@naver.com>
2024-08-07 14:56:44 +0900

    test: 바뀐 API 스펙에 맞게 명세 수정

--- backend/src/test/java/com/zzang/chongdae/global/helper/CookieProvider.java
+++ backend/src/test/java/com/zzang/chongdae/global/helper/CookieProvider.java
@@ -1,29 +1,35 @@
 package com.zzang.chongdae.global.helper;
 
-import com.zzang.chongdae.auth.service.dto.LoginRequest;
-import io.restassured.RestAssured;
+import com.zzang.chongdae.auth.service.dto.TokenDto;
+import io.restassured.http.Cookie;
 import io.restassured.http.Cookies;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class CookieProvider {
 
+    private final TestTokenProvider tokenProvider;
+
+    @Autowired
+    public CookieProvider(TestTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
     public Cookies createCookies() {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(new LoginRequest("dora1234"))
-                .when().post("/auth/login")
-                .then().log().all()
-                .extract().detailedCookies();
+        TokenDto tokenDto = tokenProvider.createTokens();
+        return createCookiesFromTokenDto(tokenDto);
     }
 
     public Cookies createCookiesWithCi(String ci) {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(new LoginRequest(ci))
-                .when().post("/auth/login")
-                .then().log().all()
-                .extract().detailedCookies();
+        TokenDto tokenDto = tokenProvider.createTokensWithCi(ci);
+        return createCookiesFromTokenDto(tokenDto);
+    }
+
+    private Cookies createCookiesFromTokenDto(TokenDto tokenDto) {
+        Cookie accessToken = new Cookie.Builder("access_token", tokenDto.accessToken()).build();
+        Cookie refreshToken = new Cookie.Builder("refresh_token", tokenDto.refreshToken()).build();
+        return new Cookies(accessToken, refreshToken);
     }
 }
