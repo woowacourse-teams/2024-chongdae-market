9a1c61db10c21ddb9e00c9a28a9864b3a4f92be0
fromitive <46563149+fromitive@users.noreply.github.com>
2024-08-07 15:37:42 +0900

    feat: n빵 가격이 낱개가격보다 큰경우 예외가 발생하도록 변경 (#202)
    * feat: n빵 가격이 낱개가격보다 큰경우 예외가 발생하도록 변경
    
    * refactor: 도메인 명칭 변경 (낱개가격 -> 원가격)
    
    * refactor: 도메인 명칭 변경 (공모 -> 댓글방)
    
    * refactor: originPrice로 http client 변경

--- backend/src/main/java/com/zzang/chongdae/offering/domain/OfferingPrice.java
+++ backend/src/main/java/com/zzang/chongdae/offering/domain/OfferingPrice.java
@@ -1,5 +1,7 @@
 package com.zzang.chongdae.offering.domain;
 
+import com.zzang.chongdae.global.exception.MarketException;
+import com.zzang.chongdae.offering.exception.OfferingErrorCode;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import lombok.AllArgsConstructor;
@@ -11,7 +13,7 @@ public class OfferingPrice {
 
     private final int totalCount;
     private final int totalPrice;
-    private final Integer eachPrice;
+    private final Integer originPrice;
 
     public int calculateDividedPrice() {
         return BigDecimal.valueOf(totalPrice)
@@ -20,6 +22,13 @@ public class OfferingPrice {
     }
 
     public double calculateDiscountRate() {
-        return (double) (eachPrice - totalPrice / totalCount) / eachPrice;
+        return (double) (originPrice - totalPrice / totalCount) / originPrice;
+    }
+
+    public void validateOriginPrice() {
+        int dividedPrice = totalPrice / totalCount;
+        if (originPrice < dividedPrice) {
+            throw new MarketException(OfferingErrorCode.CANNOT_ORIGIN_PRICE_LESS_THEN_DIVIDED_PRICE);
+        }
     }
 }
