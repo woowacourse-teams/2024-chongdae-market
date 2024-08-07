bc79c71090ceca7061df7b2a1007343bf45cd985
채현 <wjdcogus202@sookmyung.ac.kr>
2024-08-07 16:40:10 +0900

    feat: 이미지 업로드 및 권한 설정 (#216)
    * chore: 이미지 권한 추가
    
    * feat: permission manager을 생성하여 권한 체크 및 request
    
    * feat: 이미지 추가 버튼을 클릭할 시 권한 설정 연결
    
    * feat: 이미지 피커를 사용하여 uri 전달 구현
    
    * feat: 이미지 파일 업로드 api service 구현
    
    * feat: 이미지 파일 업로드 data source 구현
    
    * feat: 이미지 파일 업로드 repository 구현
    
    * feat: 이미지 파일 martipart로 변환해주는 기능 구현
    
    * feat: 이미지 업로드 관련 뷰 수정
    
    * feat: 이미지 파일 업로드 및 api 연결 구현
    
    * style: ktlint format
    
    * fix: git conflict 해결
    
    * refactor: 이미지 scaleType 변경
    
    * refactor: string value 컨벤션 적용

--- android/app/src/main/java/com/zzang/chongdae/data/source/offering/OfferingRemoteDataSource.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/source/offering/OfferingRemoteDataSource.kt
@@ -4,6 +4,7 @@ import com.zzang.chongdae.data.remote.dto.request.OfferingWriteRequest
 import com.zzang.chongdae.data.remote.dto.response.FiltersResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingsResponse
 import com.zzang.chongdae.data.remote.dto.response.ProductUrlResponse
+import okhttp3.MultipartBody
 
 interface OfferingRemoteDataSource {
     suspend fun fetchOfferings(
@@ -16,6 +17,8 @@ interface OfferingRemoteDataSource {
     suspend fun saveOffering(offeringWriteRequest: OfferingWriteRequest): Result<Unit>
 
     suspend fun saveProductImageOg(productUrl: String): Result<ProductUrlResponse>
+
+    suspend fun saveProductImageS3(image: MultipartBody.Part): Result<ProductUrlResponse>
 
     suspend fun fetchFilters(): Result<FiltersResponse>
 }
