609b45bd49db5d53dc2f5da826fb6faaa6231c26
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-07 15:50:53 +0900

    feat: 이미지 파일 업로드 api service 구현

--- android/app/src/main/java/com/zzang/chongdae/data/remote/api/OfferingApiService.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/api/OfferingApiService.kt
@@ -6,10 +6,13 @@ import com.zzang.chongdae.data.remote.dto.response.MeetingsResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingDetailResponse
 import com.zzang.chongdae.data.remote.dto.response.OfferingsResponse
 import com.zzang.chongdae.data.remote.dto.response.ProductUrlResponse
+import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -39,5 +42,11 @@ interface OfferingApiService {
     @POST("/offerings/product-images/og")
     suspend fun postProductImageOg(
         @Body productUrl: ProductUrlRequest,
+    ): Response<ProductUrlResponse>
+    
+    @Multipart
+    @POST("/offerings/product-images/s3")
+    suspend fun postProductImageS3(
+        @Part image: MultipartBody.Part,
     ): Response<ProductUrlResponse>
 }
