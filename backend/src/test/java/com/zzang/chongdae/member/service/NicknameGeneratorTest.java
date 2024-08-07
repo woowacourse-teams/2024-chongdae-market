e1304371faf8205a3ebec28dfeae1dfadefc08e1
Choo <choo000407@naver.com>
2024-08-07 15:33:10 +0900

    feat: 데이터에서 5자 이상 제거

--- backend/src/test/java/com/zzang/chongdae/member/service/NicknameGeneratorTest.java
+++ backend/src/test/java/com/zzang/chongdae/member/service/NicknameGeneratorTest.java
@@ -18,7 +18,7 @@ public class NicknameGeneratorTest {
     @Test
     void should_returnNickname_when_generateNickName() {
         // given
-        String expected = "춤추는해";
+        String expected = "춤추는도라";
 
         // when
         String actual = nickNameGenerator.generate();
