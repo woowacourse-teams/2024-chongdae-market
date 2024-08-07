67bf7159d1b03c7466d1dde647c1ff726333357c
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-07 15:51:24 +0900

    feat: 이미지 파일 업로드 repository 구현

--- android/app/src/main/java/com/zzang/chongdae/domain/repository/OfferingRepository.kt
+++ android/app/src/main/java/com/zzang/chongdae/domain/repository/OfferingRepository.kt
@@ -3,6 +3,7 @@ package com.zzang.chongdae.domain.repository
 import com.zzang.chongdae.domain.model.Offering
 import com.zzang.chongdae.domain.model.ProductUrl
 import com.zzang.chongdae.presentation.view.write.OfferingWriteUiModel
+import okhttp3.MultipartBody
 
 interface OfferingRepository {
     suspend fun fetchOfferings(
@@ -13,4 +14,6 @@ interface OfferingRepository {
     suspend fun saveOffering(uiModel: OfferingWriteUiModel): Result<Unit>
 
     suspend fun saveProductImageOg(productUrl: String): Result<ProductUrl>
+    
+    suspend fun saveProductImageS3(image: MultipartBody.Part): Result<ProductUrl>
 }
