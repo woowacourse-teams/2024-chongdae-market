d68d14814fdce3e9c9f2f8ac12c66dd04f55cc85
fromitive <46563149+fromitive@users.noreply.github.com>
2024-08-08 11:17:34 +0900

    feat: 공모 참여자 목록 조회 API 구현 (#225)
    * feat: 공모 참여자 목록 조회 API 구현
    
    * test: 실패 테스트 오류 수정
    
    * style: 띄어쓰기 적용
    
    * refactor: MemberEntity를 받도록 변경
    
    * refactor: isParticipant를 구현하여 가독성 개선
    
    * refactor: 총대를 찾을 수 없는 상황의 예외 추가
    
    * refactor: 참여 검증로직을 서비스로 이동
    
    * refactor: 사용하지 않는 메서드 제거
    
    * refactor: 검증 로직 가장 상단에 위치
    
    * refactor: 총대 추출 로직 수정
    
    ---------
    
    Co-authored-by: masonkimseoul <masonkimseoul@gmail.com>
    Co-authored-by: SCY <helenason@naver.com>

--- backend/src/main/java/com/zzang/chongdae/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/zzang/chongdae/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.zzang.chongdae.global.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +20,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorMessage> handle(MethodArgumentNotValidException e) {
         ErrorMessage errorMessage = new ErrorMessage(e.getAllErrors().get(0).getDefaultMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(errorMessage);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handle(MissingServletRequestParameterException e) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                "해당 쿼리 파리미터 값이 존재하지 않습니다 :[%s]".formatted(e.getParameterName()));
         return ResponseEntity
                 .badRequest()
                 .body(errorMessage);
---
+++ backend/src/main/java/com/zzang/chongdae/offeringmember/service/dto/ParticipantResponse.java
@@ -0,0 +1,6 @@
+package com.zzang.chongdae.offeringmember.service.dto;
+
+import java.util.List;
+
+public record ParticipantResponse(ProposerResponseItem proposer, List<ParticipantResponseItem> participants) {
+}
