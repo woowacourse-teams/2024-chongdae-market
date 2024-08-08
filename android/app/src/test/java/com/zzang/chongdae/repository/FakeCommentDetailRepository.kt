c0455b957cad1bb7d96020fb905d6a7c4f3f3bd0
채현 <wjdcogus202@sookmyung.ac.kr>
2024-08-08 20:42:26 +0900

    test: 댓글방 테스트 코드 작성  (#258)
    * refactor: 댓글 보내는 함수명 변경
    
    * refactor: 공구 약속 장소 및 시간 캐시 기능
    
    * test: 테스트를 위한 fake repository 구현
    
    * test: 댓글방 viewmodel test 작성
    
    * feat: 댓글방 ActivityTest 작성
    
    * feat: 댓글방 ActivityTest 작성
    
    * style: ktlint 적용
    
    * refactor: test fixture에서 사용하지 않는 것 삭제
    
    * style: ktlint 적용

---
+++ android/app/src/test/java/com/zzang/chongdae/repository/FakeCommentDetailRepository.kt
@@ -0,0 +1,43 @@
+package com.zzang.chongdae.repository
+
+import com.zzang.chongdae.domain.model.Comment
+import com.zzang.chongdae.domain.model.Meetings
+import com.zzang.chongdae.domain.repository.CommentDetailRepository
+import com.zzang.chongdae.util.TestFixture
+
+class FakeCommentDetailRepository : CommentDetailRepository {
+    private val comments: MutableList<Comment> = TestFixture.comments
+
+    override suspend fun fetchMeetings(offeringId: Long): Result<Meetings> {
+        return Result.success(
+            TestFixture.meetings,
+        )
+    }
+
+    override suspend fun saveComment(
+        memberId: Long,
+        offeringId: Long,
+        comment: String,
+    ): Result<Unit> {
+        comments.add(TestFixture.comment)
+        return Result.success(Unit)
+    }
+
+    override suspend fun fetchComments(
+        offeringId: Long,
+        memberId: Long,
+    ): Result<List<Comment>> {
+        return Result.success(
+            comments.toList(),
+        )
+    }
+
+    override suspend fun fetchCommentsWithRoom(
+        offeringId: Long,
+        memberId: Long,
+    ): Result<List<Comment>> {
+        return Result.success(
+            comments.toList(),
+        )
+    }
+}
