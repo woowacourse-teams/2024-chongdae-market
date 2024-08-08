0920e35201cb36563b0f5b5f4164b13e3117d70e
Namyunsuk <yunsuk0328@naver.com>
2024-08-08 22:21:19 +0900

    refactor: 클릭 시 최상단으로 이동하는 버튼 구현

--- android/app/src/main/java/com/zzang/chongdae/presentation/util/BindingAdapters.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/util/BindingAdapters.kt
@@ -131,11 +131,12 @@ private fun OfferingCondition.toOfferingComment(
     remaining: Int,
 ) = when (this) {
     OfferingCondition.FULL -> context.getString(R.string.main_offering_condition_full_comment)
-    OfferingCondition.IMMINENT -> Html.fromHtml(
-        context.getString(R.string.main_offering_condition_continue_comment)
-            .format(remaining),
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
-    )
+    OfferingCondition.IMMINENT ->
+        Html.fromHtml(
+            context.getString(R.string.main_offering_condition_continue_comment)
+                .format(remaining),
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
 
     OfferingCondition.CONFIRMED -> ""
     OfferingCondition.AVAILABLE -> ""
