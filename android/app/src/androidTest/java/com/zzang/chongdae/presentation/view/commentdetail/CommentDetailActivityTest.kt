87bd0f79642a35403b6998f1e9454d61c66f3c41
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-08 20:25:43 +0900

    feat: 댓글방 ActivityTest 작성

--- android/app/src/androidTest/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailActivityTest.kt
+++ android/app/src/androidTest/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailActivityTest.kt
@@ -1,5 +1,30 @@
 package com.zzang.chongdae.presentation.view.commentdetail
 
-import org.junit.Assert.*
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.zzang.chongdae.R
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.runner.RunWith
 
-class CommentDetailActivityTest
+@RunWith(AndroidJUnit4::class)
+class CommentDetailActivityTest{
+    private lateinit var scenario: ActivityScenario<CommentDetailActivity>
+    
+    @Before
+    fun setUp() {
+        scenario = ActivityScenario.launch(CommentDetailActivity::class.java)
+    }
+    
+    @Test
+    @DisplayName("댓글 상세 화면이 보여야 한다")
+    fun displayCommentDetailTest() {
+        // then
+        Espresso.onView(ViewMatchers.withId(R.id.view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
