be3114cb7e2c2d90b25e27bb273e5ccbcaf2f16a
Namyunsuk <yunsuk0328@naver.com>
2024-08-08 21:07:36 +0900

    feat: OfferingDetailViewModel 테스트 작성

--- android/app/src/test/java/com/zzang/chongdae/util/TestFixture.kt
+++ android/app/src/test/java/com/zzang/chongdae/util/TestFixture.kt
@@ -5,13 +5,16 @@ import com.zzang.chongdae.data.mapper.toLocalDateTime
 import com.zzang.chongdae.data.mapper.toLocalTime
 import com.zzang.chongdae.domain.model.Comment
 import com.zzang.chongdae.domain.model.CommentCreatedAt
+import com.zzang.chongdae.domain.model.CurrentCount
 import com.zzang.chongdae.domain.model.Filter
 import com.zzang.chongdae.domain.model.FilterName
 import com.zzang.chongdae.domain.model.FilterType
 import com.zzang.chongdae.domain.model.Meetings
 import com.zzang.chongdae.domain.model.Offering
 import com.zzang.chongdae.domain.model.OfferingCondition
+import com.zzang.chongdae.domain.model.OfferingDetail
 import com.zzang.chongdae.domain.model.OfferingStatus
+import java.time.LocalDateTime
 
 object TestFixture {
     val meetings: Meetings =
@@ -76,4 +79,23 @@ object TestFixture {
                 type = FilterType.VISIBLE,
             )
         }
+
+    val OFFERING_DETAIL_STUB = OfferingDetail(
+        id = 1,
+        title = "Test",
+        nickname = "Test",
+        memberId = "1L",
+        productUrl = "TEST",
+        thumbnailUrl = null,
+        dividedPrice = 1000,
+        totalPrice = 1000,
+        deadline = LocalDateTime.of(1,1,1,1,1,1,1),
+        currentCount = CurrentCount(value = 1000),
+        totalCount = 1000,
+        meetingAddress = "TEST",
+        meetingAddressDetail = "TEST",
+        description = "TEST",
+        condition = OfferingCondition.CONFIRMED,
+        isParticipated = false
+    )
 }
