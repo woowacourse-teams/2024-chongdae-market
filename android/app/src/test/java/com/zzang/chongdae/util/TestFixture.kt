4b5b127e6cd8f31122666c2f0ea06ba9439d2969
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-08-08 20:34:23 +0900

    feat: 홈화면 테스트 작성 (#257)
    * chore: mockk의존성 추가
    
    * test: OfferingViewModel 테스트 작성
    
    * style: lint적용
    
    * refactor: stub를 TestFixture로 이동

--- android/app/src/test/java/com/zzang/chongdae/util/TestFixture.kt
+++ android/app/src/test/java/com/zzang/chongdae/util/TestFixture.kt
@@ -1,3 +1,33 @@
 package com.zzang.chongdae.util
 
-object TestFixture
+import com.zzang.chongdae.domain.model.Filter
+import com.zzang.chongdae.domain.model.FilterName
+import com.zzang.chongdae.domain.model.FilterType
+import com.zzang.chongdae.domain.model.Offering
+import com.zzang.chongdae.domain.model.OfferingCondition
+
+object TestFixture {
+    val OFFERINGS_STUB =
+        (0..20).map {
+            Offering(
+                id = it.toLong(),
+                title = "",
+                meetingAddress = "",
+                thumbnailUrl = null,
+                totalCount = 0,
+                currentCount = 0,
+                dividedPrice = 0,
+                eachPrice = null,
+                condition = OfferingCondition.CONFIRMED,
+                isOpen = false,
+            )
+        }
+    val FILTERS_STUB =
+        (0..3).map {
+            Filter(
+                name = FilterName.HIGH_DISCOUNT,
+                value = "",
+                type = FilterType.VISIBLE,
+            )
+        }
+}
