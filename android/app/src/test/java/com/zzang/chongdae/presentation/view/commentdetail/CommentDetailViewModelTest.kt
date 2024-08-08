2d7d5c53c6dc616a7550dcdc67538fefa319ce57
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-08 20:21:01 +0900

    test: 댓글방 viewmodel test 작성

---
+++ android/app/src/test/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModelTest.kt
@@ -0,0 +1,116 @@
+package com.zzang.chongdae.presentation.view.commentdetail
+
+import com.zzang.chongdae.repository.FakeOfferingRepository
+import com.zzang.chongdae.domain.repository.CommentDetailRepository
+import com.zzang.chongdae.domain.repository.OfferingRepository
+import com.zzang.chongdae.repository.FakeCommentDetailRepository
+import com.zzang.chongdae.util.CoroutinesTestExtension
+import com.zzang.chongdae.util.InstantTaskExecutorExtension
+import com.zzang.chongdae.util.TestFixture
+import com.zzang.chongdae.util.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+
+@ExperimentalCoroutinesApi
+@ExtendWith(CoroutinesTestExtension::class)
+@ExtendWith(InstantTaskExecutorExtension::class)
+class CommentDetailViewModelTest {
+    private lateinit var viewModel: CommentDetailViewModel
+    private lateinit var offeringRepository: OfferingRepository
+    private lateinit var commentDetailRepository: CommentDetailRepository
+    private val offeringId: Long = 1L
+    private val offeringTitle: String = "title"
+    
+    @BeforeEach
+    fun setUp() {
+        offeringRepository = FakeOfferingRepository()
+        commentDetailRepository = FakeCommentDetailRepository()
+        viewModel = CommentDetailViewModel(offeringId, offeringTitle, offeringRepository, commentDetailRepository)
+    }
+    
+    
+    @DisplayName("공동구매 상태를 불러온다")
+    @Test
+    fun fetchOfferingStatus() {
+        // given
+        // when
+        // then
+        val result = viewModel.offeringStatusImageUrl.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.offeringStatus.imageUrl)
+    }
+    
+    @DisplayName("공동구매 상세 정보를 업데이트한다")
+    @Test
+    fun updateOffering() {
+        // given
+        viewModel.updateOfferingStatus()
+        
+        // when
+        viewModel.updateOfferingStatus()
+        // then
+        val result = viewModel.offeringStatusImageUrl.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.offeringStatus2.imageUrl)
+    }
+    
+    @DisplayName("댓글 목록을 불러온다")
+    @Test
+    fun fetchComments() {
+        // given
+        // when
+        // then
+        val result = viewModel.comments.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.comments1)
+    }
+    
+    @DisplayName("댓글을 작성하면 댓글 목록에 추가된다")
+    @Test
+    fun addComment() {
+        // given
+        val before = viewModel.comments.getOrAwaitValue()
+        assertThat(before.size).isEqualTo(1)
+        
+        // when
+        viewModel.commentContent.value = "new comment"
+        viewModel.postComment()
+        viewModel.loadComments()
+        
+        // then
+        val result = viewModel.comments.getOrAwaitValue()
+        assertThat(result.size).isEqualTo(2)
+    }
+    
+    @DisplayName("약속 장소(도로명주소)를 불러온다")
+    @Test
+    fun fetchOfferingAddress() {
+        // given
+        // when
+        // then
+        val result = viewModel.location.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.meetings.meetingAddress)
+    }
+    
+    @DisplayName("약속 장소(상세주소)를 불러온다")
+    @Test
+    fun fetchOfferingAddressDetail() {
+        // given
+        // when
+        // then
+        val result = viewModel.locationDetail.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.meetings.meetingAddressDetail)
+    }
+    
+    @DisplayName("약속 시간을 불러온다")
+    @Test
+    fun fetchOfferingDeadlineDetail() {
+        // given
+        // when
+        // then
+        val result = viewModel.deadline.getOrAwaitValue()
+        assertThat(result).isEqualTo(TestFixture.meetings.deadline)
+    }
+}
