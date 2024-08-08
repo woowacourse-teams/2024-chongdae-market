3324c226654281472e1ae670b2f1f995d6327aa2
Dora Choo <choo000407@naver.com>
2024-08-08 21:47:39 +0900

    feat: 로깅 코드 삽입 (#266)
    * fix: 원 가격이 없는 경우 n빵 가격을 비교하지 않도록 변경
    
    * feature: 로깅 샘플 구현
    
    * refactor: 불필요한 코드 제거
    
    * feat: logging 적용
    
    ---------
    
    Co-authored-by: fromitive <fromitive@gmail.com>

---
+++ backend/src/main/java/com/zzang/chongdae/logging/config/LoggingConfig.java
@@ -0,0 +1,18 @@
+package com.zzang.chongdae.logging.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@AllArgsConstructor
+@Configuration
+public class LoggingConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
---
+++ backend/src/main/java/com/zzang/chongdae/logging/config/LoggingInterceptor.java
@@ -0,0 +1,63 @@
+package com.zzang.chongdae.logging.config;
+
+import com.zzang.chongdae.global.exception.MarketException;
+import com.zzang.chongdae.logging.domain.HttpStatusCategory;
+import com.zzang.chongdae.logging.dto.LoggingInfoFailResponse;
+import com.zzang.chongdae.logging.dto.LoggingInfoSuccessResponse;
+import com.zzang.chongdae.logging.dto.LoggingWarnResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) throws IOException {
+
+        long startTime = Long.parseLong(request.getAttribute("startTime").toString());
+        long endTime = System.currentTimeMillis();
+        String latency = endTime - startTime + "ms";
+
+        String identifier = UUID.randomUUID().toString();
+        String httpMethod = request.getMethod();
+        String uri = request.getRequestURI();
+        String requestBody = new String(request.getInputStream().readAllBytes());
+
+        CachedHttpServletResponseWrapper cachedResponse = (CachedHttpServletResponseWrapper) response;
+        String statusCode = String.valueOf(cachedResponse.getStatus());
+        String responseBody = new String(cachedResponse.getCachedBody());
+
+        HttpStatusCategory statusCategory = HttpStatusCategory.fromStatusCode(cachedResponse.getStatus());
+        if (statusCategory == HttpStatusCategory.INFO_SUCCESS) {
+            LoggingInfoSuccessResponse logResponse = new LoggingInfoSuccessResponse(identifier, httpMethod, uri,
+                    requestBody, statusCode, latency);
+            log.info(logResponse.toString());
+        }
+        if (statusCategory == HttpStatusCategory.INFO_FAIL) {
+            LoggingInfoFailResponse logResponse = new LoggingInfoFailResponse(identifier, httpMethod, uri,
+                    requestBody, statusCode, responseBody, latency);
+            log.info(logResponse.toString());
+        }
+        if (statusCategory == HttpStatusCategory.WARN && ex instanceof MarketException) {
+            LoggingWarnResponse logResponse = new LoggingWarnResponse(identifier, httpMethod, uri,
+                    requestBody, statusCode, responseBody, latency);
+            log.warn(logResponse.toString());
+        }
+
+    }
+}
