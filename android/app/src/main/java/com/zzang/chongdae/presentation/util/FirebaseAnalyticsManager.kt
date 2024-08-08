acb4b6c135f42fd674d88744f41fe08a0abbaf19
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-08 16:48:25 +0900

    feat: 총대가 공구 진행 상황을 다음 단계로 변경했을 때 event 추가

--- android/app/src/main/java/com/zzang/chongdae/presentation/util/FirebaseAnalyticsManager.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/util/FirebaseAnalyticsManager.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 
 class FirebaseAnalyticsManager(private val firebaseAnalytics: FirebaseAnalytics) {
-    fun logSelectContentEvent(id: String, name: String, contentType: String) {
-        val bundle = Bundle().apply {
-            putString(FirebaseAnalytics.Param.ITEM_ID, id)
-            putString(FirebaseAnalytics.Param.ITEM_NAME, name)
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, contentType)
-        }
+    fun logSelectContentEvent(
+        id: String,
+        name: String,
+        contentType: String,
+    ) {
+        val bundle =
+            Bundle().apply {
+                putString(FirebaseAnalytics.Param.ITEM_ID, id)
+                putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+                putString(FirebaseAnalytics.Param.CONTENT_TYPE, contentType)
+            }
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
