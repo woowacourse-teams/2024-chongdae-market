0bf22966e3f6d35b3dcb201cd96d37f2a4a2a6c4
songpink <jejeje04@naver.com>
2024-08-10 19:46:45 +0900

    fix: ci 테스트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,8 @@ android {
         buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
         buildConfigField("String", "TOKEN", "\"$token\"")
         buildConfigField("String", "NATIVE_APP_KEY", "\"$nativeAppKey\"")
-//        manifestPlaceholders["native_app_key"] = nativeAppKey
+        manifestPlaceholders["native_app_key"] = nativeAppKey
+        manifestPlaceholders["native_app_key"] = properties["native_app_key"] as String
     }
 
     buildTypes {
