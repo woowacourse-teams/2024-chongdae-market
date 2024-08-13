4269b1af42ff76d6d46e366fcc80d8c7d0fb567f
Namyunsuk <yunsuk0328@naver.com>
2024-08-14 00:41:34 +0900

    refactor: 필터 단일 선택되도록 수정

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/home/OnFilterClickListener.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/home/OnFilterClickListener.kt
@@ -1,10 +1,17 @@
 package com.zzang.chongdae.presentation.view.home
 
+import android.view.View
 import com.zzang.chongdae.domain.model.FilterName
 
 interface OnFilterClickListener {
-    fun onClickFilter(
+    /*fun onClickFilter(
         filterName: FilterName,
         isChecked: Boolean,
+        button: Button,
+    )*/
+
+    fun onClickFilter(
+        filterName: FilterName,
+        button: View,
     )
 }
