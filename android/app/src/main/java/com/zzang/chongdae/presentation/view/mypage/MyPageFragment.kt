cb37358a57dd6588f6490575f9c01f1c11d75b62
채현 <wjdcogus202@sookmyung.ac.kr>
2024-08-14 17:37:14 +0900

    feat: 마이페이지 기능 구현  (#341)
    * feat: 마이페이지 닉네임 기능 구현
    
    * feat: 로그아웃 로직 구현
    
    * feat: url 연결 로직 구현
    
    * feat: 필요없는 기능 삭제
    
    * style: ktlint 적용

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageFragment.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageFragment.kt
@@ -1,5 +1,7 @@
 package com.zzang.chongdae.presentation.view.mypage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,16 +9,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.zzang.chongdae.ChongdaeApp.Companion.dataStore
+import com.zzang.chongdae.data.local.source.MemberDataStore
 import com.zzang.chongdae.databinding.FragmentMyPageBinding
 import com.zzang.chongdae.presentation.util.FirebaseAnalyticsManager
 import com.zzang.chongdae.presentation.view.MainActivity
+import com.zzang.chongdae.presentation.view.login.LoginActivity
 
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMyPageBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: MyPageViewModel by viewModels {
-        MyPageViewModel.getFactory()
+        MyPageViewModel.getFactory(MemberDataStore(requireContext().dataStore))
     }
 
     private val firebaseAnalytics: FirebaseAnalytics by lazy {
@@ -54,6 +59,28 @@ class MyPageFragment : Fragment() {
     }
 
     private fun setUpObserve() {
+        viewModel.openUrlInBrowserEvent.observe(viewLifecycleOwner) {
+            openUrlInBrowser(it)
+        }
+        viewModel.logoutEvent.observe(viewLifecycleOwner) {
+            clearDataAndLogout()
+        }
+    }
+
+    private fun clearDataAndLogout() {
+        val intent =
+            Intent(requireContext(), LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+        startActivity(intent)
+    }
+
+    private fun openUrlInBrowser(url: String) {
+        val intent =
+            Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
+            }
+        startActivity(intent)
     }
 
     override fun onResume() {
--- android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
@@ -1,24 +1,54 @@
 package com.zzang.chongdae.presentation.view.mypage
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.zzang.chongdae.data.local.source.MemberDataStore
+import com.zzang.chongdae.presentation.util.MutableSingleLiveData
+import com.zzang.chongdae.presentation.util.SingleLiveData
 
-class MyPageViewModel() : ViewModel() {
-    private val _nickname = MutableLiveData<String>()
-    val nickname: LiveData<String> = _nickname
+class MyPageViewModel(private val memberDataStore: MemberDataStore) : ViewModel() {
+    val nickName: LiveData<String?> = memberDataStore.nickNameFlow.asLiveData()
+
+    private val _openUrlInBrowserEvent = MutableSingleLiveData<String>()
+    val openUrlInBrowserEvent: SingleLiveData<String> get() = _openUrlInBrowserEvent
+
+    private val _logoutEvent = MutableSingleLiveData<Unit>()
+    val logoutEvent: SingleLiveData<Unit> get() = _logoutEvent
+
+    private val termsOfUseUrl =
+        "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
+    private val privacyUrl =
+        "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
+    private val withdrawalUrl = "https://forms.gle/z5MUzVTUoyunfqEu8"
+
+    fun onClickTermsOfUse() {
+        _openUrlInBrowserEvent.setValue(termsOfUseUrl)
+    }
+
+    fun onClickPrivacy() {
+        _openUrlInBrowserEvent.setValue(privacyUrl)
+    }
+
+    fun onClickLogout() {
+        _logoutEvent.setValue(Unit)
+    }
+
+    fun onClickWithdrawal() {
+        _openUrlInBrowserEvent.setValue(withdrawalUrl)
+    }
 
     companion object {
         @Suppress("UNCHECKED_CAST")
-        fun getFactory() =
+        fun getFactory(memberDataStore: MemberDataStore) =
             object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(
                     modelClass: Class<T>,
                     extras: CreationExtras,
                 ): T {
-                    return MyPageViewModel() as T
+                    return MyPageViewModel(memberDataStore) as T
                 }
             }
     }
