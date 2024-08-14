d898839c148c0a83a5a5f1843c88eb023e0c2a52
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-14 16:39:41 +0900

    style: ktlint 적용

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
@@ -4,12 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.zzang.chongdae.data.local.source.MemberDataStore
 import com.zzang.chongdae.presentation.util.MutableSingleLiveData
 import com.zzang.chongdae.presentation.util.SingleLiveData
-import kotlinx.coroutines.launch
 
 class MyPageViewModel(private val memberDataStore: MemberDataStore) : ViewModel() {
     val nickName: LiveData<String?> = memberDataStore.nickNameFlow.asLiveData()
