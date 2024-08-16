b1cafbeba6729b02ebe00eaa1db7f60cca9d94a2
SCY <helenason@naver.com>
2024-08-16 17:03:00 +0900

    refactor: 할인율 계산 로직 수정 (#359)
    * refactor: 할인율 계산 로직 수정
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    
    * refactor: 소수점 둘째 자리에서 반올림하도록 변경
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    
    * test: 할인율 계산 로직
    
    * fix: 할인율 단위 백분율로 수정
    
    ---------
    
    Co-authored-by: fromitive <fromitive@gmail.com>

--- backend/src/main/java/com/zzang/chongdae/offering/service/dto/OfferingSaveRequest.java
+++ backend/src/main/java/com/zzang/chongdae/offering/service/dto/OfferingSaveRequest.java
@@ -3,6 +3,7 @@ package com.zzang.chongdae.offering.service.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zzang.chongdae.member.repository.entity.MemberEntity;
 import com.zzang.chongdae.offering.domain.CommentRoomStatus;
+import com.zzang.chongdae.offering.domain.OfferingPrice;
 import com.zzang.chongdae.offering.domain.OfferingStatus;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
 import jakarta.validation.constraints.NotBlank;
@@ -39,9 +40,12 @@ public record OfferingSaveRequest(@NotBlank
                                   String description) {
 
     public OfferingEntity toEntity(MemberEntity member) {
+        OfferingPrice offeringPrice = new OfferingPrice(totalCount, totalPrice, originPrice);
+        Double discountRate = offeringPrice.calculateDiscountRate();
+
         return new OfferingEntity(member, title, description, thumbnailUrl, productUrl, meetingDate, meetingAddress,
                 meetingAddressDetail, meetingAddressDong, totalCount, 1, false,
-                totalPrice, originPrice, 33.3, OfferingStatus.AVAILABLE, CommentRoomStatus.GROUPING);
+                totalPrice, originPrice, discountRate, OfferingStatus.AVAILABLE, CommentRoomStatus.GROUPING);
         // TODO : 각자 맡은 팀에서 계산하는 로직 생각하기, 객체로 빼서 만드셈~
     }
 }
