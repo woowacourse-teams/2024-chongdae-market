37f47c74336a7074767f1e36aefe515130d5b1c1
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-08-16 17:05:25 +0900

    fix: 필터 오류 수정 (#362)
    * fix: 필터 오류 수정
    
    - '참여가능만'필터 분기처리 제거
    
    * chore: 주석 제거

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/home/OnFilterClickListener.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/home/OnFilterClickListener.kt
@@ -1,17 +1,10 @@
 package com.zzang.chongdae.presentation.view.home
 
-import android.view.View
 import com.zzang.chongdae.domain.model.FilterName
 
 interface OnFilterClickListener {
-    /*fun onClickFilter(
-        filterName: FilterName,
-        isChecked: Boolean,
-        button: Button,
-    )*/
-
     fun onClickFilter(
         filterName: FilterName,
-        button: View,
+        isChecked: Boolean,
     )
 }
