c06acfb77a2bc1cfbbd3bb6ba5c8de76f5f36076
SCY <helenason@naver.com>
2024-08-19 21:12:44 +0900

    test: 공모 단건 조회 서비스 테스트

--- backend/src/test/java/com/zzang/chongdae/offering/service/OfferingServiceTest.java
+++ backend/src/test/java/com/zzang/chongdae/offering/service/OfferingServiceTest.java
@@ -1,10 +1,15 @@
 package com.zzang.chongdae.offering.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.zzang.chongdae.global.exception.MarketException;
 import com.zzang.chongdae.global.service.ServiceTest;
 import com.zzang.chongdae.member.repository.entity.MemberEntity;
+import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
+import com.zzang.chongdae.offering.service.dto.OfferingAllResponseItem;
 import com.zzang.chongdae.offering.service.dto.OfferingSaveRequest;
 import java.time.LocalDateTime;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +19,38 @@ public class OfferingServiceTest extends ServiceTest {
     @Autowired
     OfferingService offeringService;
 
+    @DisplayName("공모 id를 통해 공모를 단건 조회할 수 있다.")
+    @Test
+    void should_getOffering_when_givenOfferingId() {
+        // given
+        MemberEntity member = memberFixture.createMember("ever");
+        OfferingEntity offering = offeringFixture.createOffering(member);
+        OfferingAllResponseItem expected = new OfferingAllResponseItem(offering, offering.toOfferingPrice());
+
+        // when
+        OfferingAllResponseItem actual = offeringService.getOffering(offering.getId());
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("유효하지 않은 공모 id를 통해 공모를 단건 조회할 경우 예외가 발생한다.")
+    @Test
+    void should_throwException_when_givenInvalidOfferingId() {
+        // given
+        MemberEntity member = memberFixture.createMember("ever");
+        OfferingEntity offering = offeringFixture.createOffering(member);
+
+        // when & then
+        long invalidOfferingId = offering.getId() + 9999;
+
+        assertThatThrownBy(() -> offeringService.getOffering(invalidOfferingId))
+                .isInstanceOf(MarketException.class);
+    }
+
     @DisplayName("공목 등록 시 원 가격 정보가 없더라도 공모 작성에 성공할 수 있다.")
     @Test
     void should_createOffering_when_givenOfferingWithoutOriginPriceCreateRequest() {
-
         // given
         MemberEntity member = memberFixture.createMember("pizza");
         OfferingSaveRequest request = new OfferingSaveRequest(
@@ -39,6 +72,6 @@ public class OfferingServiceTest extends ServiceTest {
         Long actual = offeringService.saveOffering(request, member);
 
         // then
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }
