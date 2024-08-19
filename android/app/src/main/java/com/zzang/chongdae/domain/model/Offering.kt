d299285c44a0f04b48ab8d09660d225fd35ef19d
songpink <jejeje04@naver.com>
2024-08-19 18:08:51 +0900

    feat: 공모 목록에서 동을 보여주는 기능 구현

--- android/app/src/main/java/com/zzang/chongdae/domain/model/Offering.kt
+++ android/app/src/main/java/com/zzang/chongdae/domain/model/Offering.kt
@@ -3,7 +3,7 @@ package com.zzang.chongdae.domain.model
 data class Offering(
     val id: Long,
     val title: String,
-    val meetingAddress: String,
+    val meetingAddressDong: String,
     val thumbnailUrl: String?,
     val totalCount: Int,
     val currentCount: Int,
