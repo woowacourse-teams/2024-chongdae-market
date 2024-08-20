981ce426fc676ec4a7dd08fd5538b0c4eed59037
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-20 13:08:27 +0900

    test: 코드 변경에 따른 테스트 코드 수정

--- android/app/src/test/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModelTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/presentation/view/commentdetail/CommentDetailViewModelTest.kt
@@ -3,9 +3,11 @@ package com.zzang.chongdae.presentation.view.commentdetail
 import com.zzang.chongdae.domain.repository.AuthRepository
 import com.zzang.chongdae.domain.repository.CommentDetailRepository
 import com.zzang.chongdae.domain.repository.OfferingRepository
+import com.zzang.chongdae.domain.repository.ParticipantRepository
 import com.zzang.chongdae.repository.FakeAuthRepository
 import com.zzang.chongdae.repository.FakeCommentDetailRepository
 import com.zzang.chongdae.repository.FakeOfferingRepository
+import com.zzang.chongdae.repository.FakeParticipantRepository
 import com.zzang.chongdae.util.CoroutinesTestExtension
 import com.zzang.chongdae.util.InstantTaskExecutorExtension
 import com.zzang.chongdae.util.TestFixture
@@ -24,16 +26,17 @@ class CommentDetailViewModelTest {
     private lateinit var viewModel: CommentDetailViewModel
     private lateinit var authRepository: AuthRepository
     private lateinit var offeringRepository: OfferingRepository
+    private lateinit var participantRepository: ParticipantRepository
     private lateinit var commentDetailRepository: CommentDetailRepository
     private val offeringId: Long = 1L
-    private val offeringTitle: String = "title"
 
     @BeforeEach
     fun setUp() {
         authRepository = FakeAuthRepository()
         offeringRepository = FakeOfferingRepository()
+        participantRepository = FakeParticipantRepository()
         commentDetailRepository = FakeCommentDetailRepository()
-        viewModel = CommentDetailViewModel(authRepository, offeringId, offeringTitle, offeringRepository, commentDetailRepository)
+        viewModel = CommentDetailViewModel(offeringId,  offeringRepository,participantRepository, commentDetailRepository)
     }
 
     @DisplayName("공동구매 상태를 불러온다")
@@ -42,8 +45,8 @@ class CommentDetailViewModelTest {
         // given
         // when
         // then
-        val result = viewModel.offeringStatusImageUrl.getOrAwaitValue()
-        assertThat(result).isEqualTo(TestFixture.commentOfferingInfo.imageUrl)
+        val result = viewModel.commentOfferingInfo.getOrAwaitValue()
+        assertThat(result.imageUrl).isEqualTo(TestFixture.commentOfferingInfo.imageUrl)
     }
 
     @DisplayName("공동구매 상세 정보를 업데이트한다")
@@ -55,8 +58,8 @@ class CommentDetailViewModelTest {
         // when
         viewModel.updateOfferingStatus()
         // then
-        val result = viewModel.offeringStatusImageUrl.getOrAwaitValue()
-        assertThat(result).isEqualTo(TestFixture.commentOfferingInfo2.imageUrl)
+        val result = viewModel.commentOfferingInfo.getOrAwaitValue()
+        assertThat(result.imageUrl).isEqualTo(TestFixture.commentOfferingInfo2.imageUrl)
     }
 
     @DisplayName("댓글 목록을 불러온다")
@@ -92,8 +95,8 @@ class CommentDetailViewModelTest {
         // given
         // when
         // then
-        val result = viewModel.location.getOrAwaitValue()
-        assertThat(result).isEqualTo(TestFixture.meetings.meetingAddress)
+        val result = viewModel.meetings.getOrAwaitValue()
+        assertThat(result.meetingAddress).isEqualTo(TestFixture.meetings.meetingAddress)
     }
 
     @DisplayName("약속 장소(상세주소)를 불러온다")
@@ -102,8 +105,8 @@ class CommentDetailViewModelTest {
         // given
         // when
         // then
-        val result = viewModel.locationDetail.getOrAwaitValue()
-        assertThat(result).isEqualTo(TestFixture.meetings.meetingAddressDetail)
+        val result = viewModel.meetings.getOrAwaitValue()
+        assertThat(result.meetingAddressDetail).isEqualTo(TestFixture.meetings.meetingAddressDetail)
     }
 
     @DisplayName("약속 시간을 불러온다")
@@ -112,7 +115,7 @@ class CommentDetailViewModelTest {
         // given
         // when
         // then
-        val result = viewModel.deadline.getOrAwaitValue()
-        assertThat(result).isEqualTo(TestFixture.meetings.meetingDate)
+        val result = viewModel.meetings.getOrAwaitValue()
+        assertThat(result.meetingDate).isEqualTo(TestFixture.meetings.meetingDate)
     }
 }
