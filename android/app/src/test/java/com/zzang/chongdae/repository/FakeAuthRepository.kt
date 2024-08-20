945afe24851b21aaa260fb63827f0d6005b65feb
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-20 21:55:28 +0900

    test: 코드 변경에 따른 Fake Repository 변경

--- android/app/src/test/java/com/zzang/chongdae/repository/FakeAuthRepository.kt
+++ android/app/src/test/java/com/zzang/chongdae/repository/FakeAuthRepository.kt
@@ -2,17 +2,19 @@ package com.zzang.chongdae.repository
 
 import com.zzang.chongdae.domain.model.Member
 import com.zzang.chongdae.domain.repository.AuthRepository
+import com.zzang.chongdae.domain.util.DataError
+import com.zzang.chongdae.domain.util.Result
 
 class FakeAuthRepository : AuthRepository {
-    override suspend fun saveLogin(ci: String): Result<Member> {
+    override suspend fun saveLogin(ci: String): Result<Member, DataError.Network> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun saveRefresh(): Result<Unit> {
+    override suspend fun saveRefresh(): Result<Unit, DataError.Network> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun saveSignup(ci: String): Result<Member> {
+    override suspend fun saveSignup(ci: String): Result<Member, DataError.Network> {
         TODO("Not yet implemented")
     }
 }
