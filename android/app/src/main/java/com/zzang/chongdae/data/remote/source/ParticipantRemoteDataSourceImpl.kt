e72c28d647666f5631bc5f559a39f600bc8e441b
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-20 11:05:37 +0900

    feat: 참여 관리 datasource 구현

---
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/source/ParticipantRemoteDataSourceImpl.kt
@@ -0,0 +1,26 @@
+package com.zzang.chongdae.data.remote.source
+
+import com.zzang.chongdae.data.remote.api.ParticipationApiService
+import com.zzang.chongdae.data.remote.dto.response.participants.ParticipantsResponse
+import com.zzang.chongdae.data.source.ParticipantRemoteDataSource
+import retrofit2.Response
+
+class ParticipantRemoteDataSourceImpl(
+    private val service: ParticipationApiService,
+) : ParticipantRemoteDataSource {
+    override suspend fun fetchParticipants(offeringId: Long): Result<ParticipantsResponse> =
+        runCatching {
+            val response: Response<ParticipantsResponse> =
+                service.getParticipants(offeringId)
+            if (response.isSuccessful) {
+                response.body() ?: error(ERROR_PREFIX + ERROR_NULL_MESSAGE)
+            } else {
+                error(ERROR_PREFIX + response.code())
+            }
+        }
+
+    companion object {
+        const val ERROR_PREFIX = "에러 발생: "
+        const val ERROR_NULL_MESSAGE = "null"
+    }
+}
