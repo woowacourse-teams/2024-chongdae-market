86ea424a1481ca192dba69babad32e6ac99c28b8
songpink <jejeje04@naver.com>
2024-08-20 16:15:06 +0900

    refactor: Result 변경에 따른 레포지토리 수정 (AuthRepository, CommentRoomsRepository)

--- android/app/src/main/java/com/zzang/chongdae/domain/repository/AuthRepository.kt
+++ android/app/src/main/java/com/zzang/chongdae/domain/repository/AuthRepository.kt
@@ -1,11 +1,13 @@
 package com.zzang.chongdae.domain.repository
 
 import com.zzang.chongdae.domain.model.Member
+import com.zzang.chongdae.domain.util.DataError
+import com.zzang.chongdae.domain.util.Result
 
 interface AuthRepository {
-    suspend fun saveLogin(ci: String): Result<Member>
+    suspend fun saveLogin(ci: String): Result<Member, DataError.Network>
 
-    suspend fun saveRefresh(): Result<Unit>
+    suspend fun saveRefresh(): Result<Unit, DataError.Network>
 
-    suspend fun saveSignup(ci: String): Result<Member>
+    suspend fun saveSignup(ci: String): Result<Member, DataError.Network>
 }
