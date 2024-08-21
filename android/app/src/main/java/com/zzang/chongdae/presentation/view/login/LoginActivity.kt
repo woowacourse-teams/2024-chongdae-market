49ab864fa9094e29935a48d1f8f8f7e7068da503
songpink <jejeje04@naver.com>
2024-08-21 20:24:49 +0900

    feat: 테스트

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/login/LoginActivity.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/login/LoginActivity.kt
@@ -113,6 +113,7 @@ class LoginActivity : AppCompatActivity(), OnAuthClickListener {
 
     private fun isKakaoTalkLoginCanceled(error: Throwable?): Boolean {
         return error is ClientError && error.reason == ClientErrorCause.Cancelled
+        Log.d("alsong", "       fgggsg ")
     }
 
     private fun loadUserInformation() {
