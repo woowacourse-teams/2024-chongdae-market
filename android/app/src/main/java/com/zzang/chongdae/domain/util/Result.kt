67effd065bc8a991655a44c8764081defd38facf
Namyunsuk <84739562+Namyunsuk@users.noreply.github.com>
2024-08-22 16:40:53 +0900

    refactor: 에러 핸들링 리팩토링 (#436)

--- android/app/src/main/java/com/zzang/chongdae/domain/util/Result.kt
+++ android/app/src/main/java/com/zzang/chongdae/domain/util/Result.kt
@@ -5,17 +5,17 @@ typealias RootError = Error
 sealed interface Result<out D, out E : RootError> {
     data class Success<out D, out E : RootError>(val data: D) : Result<D, E>
 
-    data class Error<out D, out E : RootError>(val error: E) : Result<D, E>
+    data class Error<out E : RootError>(val msg: String, val error: E) : Result<Nothing, E>
 
     fun <R> map(transform: (D) -> R): Result<R, E> =
         when (this) {
             is Success -> Success(transform(data))
-            is Error -> Error(error)
+            is Error -> this
         }
 
     fun getOrThrow(): D =
         when (this) {
             is Success -> data
-            is Error -> throw RuntimeException("Error occurred: $error")
+            is Error -> throw RuntimeException("Error occurred: $msg, $error")
         }
 }
