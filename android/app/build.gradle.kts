5b19b653a4b3d915bf9c9da48b8d38ecc25174f2
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-08-22 17:28:09 +0900

    chore: 버전 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.zzang.chongdae"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
