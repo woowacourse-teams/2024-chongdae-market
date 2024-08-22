261e5eb188ba84c0c997761f8a794c9b6b216b39
fromitive <46563149+fromitive@users.noreply.github.com>
2024-08-22 15:51:20 +0900

    feat: 로깅 시 UUID가 아닌 회원 번호가 기록되도록 변경 (#428)
    * feat: logging 시 memberId가 나오도록 기능 추가
    
    * feat: logging 시 memberId 및 identifier가 함께 나오도록 변경
    
    * refactor: lombok getter 적용

--- backend/src/main/java/com/zzang/chongdae/logging/dto/LoggingInfoFailResponse.java
+++ backend/src/main/java/com/zzang/chongdae/logging/dto/LoggingInfoFailResponse.java
@@ -1,6 +1,7 @@
 package com.zzang.chongdae.logging.dto;
 
 public record LoggingInfoFailResponse(String identifier,
+                                      String memberIdentifier,
                                       String httpMethod,
                                       String uri,
                                       String requestBody,
