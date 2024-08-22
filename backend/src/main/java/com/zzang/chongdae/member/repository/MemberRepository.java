c4ca2eb9304be6b0137542d5f29fadc8d9b9ab19
Dora Choo <choo000407@naver.com>
2024-08-22 14:21:23 +0900

    feat: 카카오 로그인 중 사용자 정보 확인 로직을 안드로이드에서 백엔드로 이관 (#404)
    * feat: 카카오 로그인 API 구현
    
    * feat: providerId를 loginId로 수정
    
    * feat: 소셜 로그인 시 랜덤 생성된 비밀번호 사용
    
    * refactor: 불필요한 api 제거
    
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>
    Co-authored-by: SCY <helenason@naver.com>
    
    * test: 로그인 로직 변경
    
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>
    Co-authored-by: SCY <helenason@naver.com>
    
    * test: MemberFixture 불필요한 함수 제거 및 통일
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * refactor: 불필요한 정보 제거
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: 카카오 로그인 에러 핸들러 추가
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    * feat: 민감 정보 로깅에서 제외
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    Co-authored-by: Dora Choo <choo000407@naver.com>
    
    ---------
    
    Co-authored-by: fromitive <46563149+fromitive@users.noreply.github.com>
    Co-authored-by: SCY <helenason@naver.com>
    Co-authored-by: fromitive <fromitive@gmail.com>

--- backend/src/main/java/com/zzang/chongdae/member/repository/MemberRepository.java
+++ backend/src/main/java/com/zzang/chongdae/member/repository/MemberRepository.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
     Optional<MemberEntity> findByPassword(String password);
 
     boolean existsByPassword(String password);
 
     boolean existsByNickname(String nickname);
+
+    Optional<MemberEntity> findByLoginId(String loginId);
 }
