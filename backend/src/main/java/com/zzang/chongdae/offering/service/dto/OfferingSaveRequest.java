a69851f127026d0aa4ad7ab742af622fa10f83fc
masonkimseoul <masonkimseoul@gmail.com>
2024-09-23 16:50:15 +0900

    refactor: 검증 로직을 서비스로 이동

--- backend/src/main/java/com/zzang/chongdae/offering/service/dto/OfferingSaveRequest.java
+++ backend/src/main/java/com/zzang/chongdae/offering/service/dto/OfferingSaveRequest.java
@@ -1,16 +1,13 @@
 package com.zzang.chongdae.offering.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.zzang.chongdae.global.exception.MarketException;
 import com.zzang.chongdae.member.repository.entity.MemberEntity;
 import com.zzang.chongdae.offering.domain.CommentRoomStatus;
 import com.zzang.chongdae.offering.domain.OfferingPrice;
 import com.zzang.chongdae.offering.domain.OfferingStatus;
-import com.zzang.chongdae.offering.exception.OfferingErrorCode;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record OfferingSaveRequest(@NotBlank
@@ -43,20 +40,11 @@ public record OfferingSaveRequest(@NotBlank
                                   String description) {
 
     public OfferingEntity toEntity(MemberEntity member) {
-        validateMeetingDate();
         OfferingPrice offeringPrice = new OfferingPrice(totalCount, totalPrice, originPrice);
         Double discountRate = offeringPrice.calculateDiscountRate();
 
         return new OfferingEntity(member, title, description, thumbnailUrl, productUrl, meetingDate, meetingAddress,
                 meetingAddressDetail, meetingAddressDong, totalCount, 1,
                 totalPrice, originPrice, discountRate, OfferingStatus.AVAILABLE, CommentRoomStatus.GROUPING);
-        // TODO : 각자 맡은 팀에서 계산하는 로직 생각하기, 객체로 빼서 만드셈~
-    }
-
-    private void validateMeetingDate() {
-        LocalDate thresholdDate = LocalDate.now().plusDays(1);
-        if (meetingDate.toLocalDate().isBefore(thresholdDate)) {
-            throw new MarketException(OfferingErrorCode.CANNOT_MEETING_DATE_BEFORE_THAN_TOMORROW);
-        }
     }
 }
