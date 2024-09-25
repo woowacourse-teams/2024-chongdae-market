707a9a8b84af84d8751ccccd6f21505d8b94f1f6
songpink <jejeje04@naver.com>
2024-09-26 01:13:36 +0900

    feat: (빠트린것) MyPageViewModel에 Hilt 적용

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/mypage/MyPageViewModel.kt
@@ -9,52 +9,59 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import com.zzang.chongdae.common.datastore.UserPreferencesDataStore
 import com.zzang.chongdae.presentation.util.MutableSingleLiveData
 import com.zzang.chongdae.presentation.util.SingleLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MyPageViewModel(private val userPreferencesDataStore: UserPreferencesDataStore) : ViewModel() {
-    val nickName: LiveData<String?> = userPreferencesDataStore.nickNameFlow.asLiveData()
+@HiltViewModel
+class MyPageViewModel
+    @Inject
+    constructor(
+        private val userPreferencesDataStore: UserPreferencesDataStore,
+    ) : ViewModel() {
+        val nickName: LiveData<String?> = userPreferencesDataStore.nickNameFlow.asLiveData()
 
-    private val _openUrlInBrowserEvent = MutableSingleLiveData<String>()
-    val openUrlInBrowserEvent: SingleLiveData<String> get() = _openUrlInBrowserEvent
+        private val _openUrlInBrowserEvent = MutableSingleLiveData<String>()
+        val openUrlInBrowserEvent: SingleLiveData<String> get() = _openUrlInBrowserEvent
 
-    private val _logoutEvent = MutableSingleLiveData<Unit>()
-    val logoutEvent: SingleLiveData<Unit> get() = _logoutEvent
+        private val _logoutEvent = MutableSingleLiveData<Unit>()
+        val logoutEvent: SingleLiveData<Unit> get() = _logoutEvent
 
-    private val termsOfUseUrl =
-        "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
-    private val privacyUrl =
-        "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
-    private val withdrawalUrl = "https://forms.gle/z5MUzVTUoyunfqEu8"
+        private val termsOfUseUrl =
+            "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
+        private val privacyUrl =
+            "https://silent-apparatus-578.notion.site/f1f5cd1609d4469dba3ab7d0f95c183c?pvs=4"
+        private val withdrawalUrl = "https://forms.gle/z5MUzVTUoyunfqEu8"
 
-    fun onClickTermsOfUse() {
-        _openUrlInBrowserEvent.setValue(termsOfUseUrl)
-    }
-
-    fun onClickPrivacy() {
-        _openUrlInBrowserEvent.setValue(privacyUrl)
-    }
-
-    fun onClickLogout() {
-        viewModelScope.launch {
-            userPreferencesDataStore.removeAllData()
+        fun onClickTermsOfUse() {
+            _openUrlInBrowserEvent.setValue(termsOfUseUrl)
         }
-        _logoutEvent.setValue(Unit)
-    }
 
-    fun onClickWithdrawal() {
-        _openUrlInBrowserEvent.setValue(withdrawalUrl)
-    }
+        fun onClickPrivacy() {
+            _openUrlInBrowserEvent.setValue(privacyUrl)
+        }
 
-    companion object {
-        @Suppress("UNCHECKED_CAST")
-        fun getFactory(userPreferencesDataStore: UserPreferencesDataStore) =
-            object : ViewModelProvider.Factory {
-                override fun <T : ViewModel> create(
-                    modelClass: Class<T>,
-                    extras: CreationExtras,
-                ): T {
-                    return MyPageViewModel(userPreferencesDataStore) as T
-                }
+        fun onClickLogout() {
+            viewModelScope.launch {
+                userPreferencesDataStore.removeAllData()
             }
+            _logoutEvent.setValue(Unit)
+        }
+
+        fun onClickWithdrawal() {
+            _openUrlInBrowserEvent.setValue(withdrawalUrl)
+        }
+
+        companion object {
+            @Suppress("UNCHECKED_CAST")
+            fun getFactory(userPreferencesDataStore: UserPreferencesDataStore) =
+                object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(
+                        modelClass: Class<T>,
+                        extras: CreationExtras,
+                    ): T {
+                        return MyPageViewModel(userPreferencesDataStore) as T
+                    }
+                }
+        }
     }
-}
