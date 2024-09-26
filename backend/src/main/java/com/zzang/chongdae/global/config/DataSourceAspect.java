91b33d75fbe77e68337b4eeada12de98eaa51cd9
Choo <choo000407@naver.com>
2024-09-26 21:01:39 +0900

    feat: transactional을 writerDatabase 어노테이션으로 대체

--- backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
+++ backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
@@ -4,18 +4,13 @@ package com.zzang.chongdae.global.config;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Aspect
 @Component
 public class DataSourceAspect {
 
-    @Before("@annotation(org.springframework.transaction.annotation.Transactional) && @annotation(transactional)")
-    public void setDataSource(Transactional transactional) {
-        if (transactional.readOnly()) {
-            DataSourceRouter.setDataSourceKey("read");
-            return;
-        }
+    @Before("@annotation(writerDatabase)")
+    public void setDataSource(WriterDatabase writerDatabase) {
         DataSourceRouter.setDataSourceKey("write");
     }
 }
