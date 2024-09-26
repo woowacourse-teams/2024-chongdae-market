ccff6421f45a430ea3f5f3680b58fb9fe589e1ac
Choo <choo000407@naver.com>
2024-09-26 21:04:15 +0900

    feat: DataSource 관련 동작 prod 프로필로 제한

--- backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
+++ backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
@@ -3,8 +3,10 @@ package com.zzang.chongdae.global.config;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("prod")
 @Aspect
 @Component
 public class DataSourceAspect {
