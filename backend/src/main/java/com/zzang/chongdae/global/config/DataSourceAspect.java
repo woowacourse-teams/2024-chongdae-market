8ca79d3152347062c25f7bdb54076b4326b4556d
Choo <choo000407@naver.com>
2024-09-26 21:20:43 +0900

    feat: writerDatasource 사용 후 readerDatasource로 변경

--- backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
+++ backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
@@ -1,6 +1,7 @@
 package com.zzang.chongdae.global.config;
 
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Profile;
@@ -12,7 +13,12 @@ import org.springframework.stereotype.Component;
 public class DataSourceAspect {
 
     @Before("@annotation(writerDatabase)")
-    public void setDataSource(WriterDatabase writerDatabase) {
+    public void setWriterDataSource(WriterDatabase writerDatabase) {
         DataSourceRouter.setDataSourceKey("write");
+    }
+
+    @After("@annotation(writerDatabase)")
+    public void setReaderDataSource(WriterDatabase writerDatabase) {
+        DataSourceRouter.setDataSourceKey("read");
     }
 }
