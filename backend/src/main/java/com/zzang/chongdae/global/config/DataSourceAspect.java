6e712e61eaee459c948e266ecdb93a4f2a40f7ff
Choo <choo000407@naver.com>
2024-09-27 12:57:48 +0900

    chore: osiv 꺼서 인증필터가 사용한 datasource의 영향 제거

--- backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
+++ backend/src/main/java/com/zzang/chongdae/global/config/DataSourceAspect.java
@@ -1,12 +1,14 @@
 package com.zzang.chongdae.global.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Profile("prod")
 @Aspect
 @Component
@@ -14,11 +16,13 @@ public class DataSourceAspect {
 
     @Before("@annotation(writerDatabase)")
     public void setWriterDataSource(WriterDatabase writerDatabase) {
+        log.info("AOP 시작: Router를 write로 세팅");
         DataSourceRouter.setDataSourceKey("write");
     }
 
     @After("@annotation(writerDatabase)")
     public void setReaderDataSource(WriterDatabase writerDatabase) {
+        log.info("AOP 마무리: Router를 read로 세팅");
         DataSourceRouter.setDataSourceKey("read");
     }
 }
--- backend/src/main/java/com/zzang/chongdae/global/config/DataSourceRouter.java
+++ backend/src/main/java/com/zzang/chongdae/global/config/DataSourceRouter.java
@@ -1,20 +1,21 @@
 package com.zzang.chongdae.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+@Slf4j
 public class DataSourceRouter extends AbstractRoutingDataSource {
+
     private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
 
     public static void setDataSourceKey(String key) {
         contextHolder.set(key);
     }
 
-    public static void clearDataSourceKey() {
-        contextHolder.remove();
-    }
-
     @Override
     protected Object determineCurrentLookupKey() {
-        return contextHolder.get();
+        Object object = contextHolder.get();
+        log.info("datasourceRounter에서 look up key로 사용할 datasource를 결정한다: {}", object);
+        return object;
     }
 }
