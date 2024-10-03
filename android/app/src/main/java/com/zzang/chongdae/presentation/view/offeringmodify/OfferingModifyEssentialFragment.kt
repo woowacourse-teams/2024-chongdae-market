349c1af7c51db1205959663e11f4a68d5cf0e6e0
songpink <jejeje04@naver.com>
2024-10-03 20:31:41 +0900

    style: ktFormat 적용

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/offeringmodify/OfferingModifyEssentialFragment.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/offeringmodify/OfferingModifyEssentialFragment.kt
@@ -2,7 +2,6 @@ package com.zzang.chongdae.presentation.view.offeringmodify
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
