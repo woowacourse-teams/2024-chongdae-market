ee4eaec633ac2a8ada36a0dc0b5326631fc468a0
Dora Choo <choo000407@naver.com>
2024-10-07 13:21:43 +0900

    feat: 채팅방 조회 시 soft delete 된 공모 별도 처리 (#502)
    * feat: 채팅방 상세 조회 시 isDeleted 필드 추가
    
    * feat: 채팅방 목록 조회 시 삭제된 공모 별도 처리
    
    * feat: Comment-Offering 사이에도 FetchType.LAZY 적용
    
    * refactor: 불필요한 nativeQuery 사용 제거
    
    * refactor: 직접적인 필드 아닌 도메인은 서비스에서 넘겨주도록 수정
    
    * refactor: 불필요한 기존 로직 제거
    
    * docs: httpClient에 추가된 api 반영
    
    * chore: local 환경에서 h2 console 활성화
    
    * feat: isDeleted 필드 제거

---
+++ backend/src/test/java/com/zzang/chongdae/comment/service/CommentServiceTest.java
@@ -0,0 +1,98 @@
+package com.zzang.chongdae.comment.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.zzang.chongdae.comment.service.dto.CommentRoomAllResponse;
+import com.zzang.chongdae.comment.service.dto.CommentRoomInfoResponse;
+import com.zzang.chongdae.global.service.ServiceTest;
+import com.zzang.chongdae.member.repository.entity.MemberEntity;
+import com.zzang.chongdae.offering.domain.CommentRoomStatus;
+import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CommentServiceTest extends ServiceTest {
+
+    @Autowired
+    CommentService commentService;
+
+    @DisplayName("댓글방 목록 조회")
+    @Nested
+    class GetAllCommentRoom {
+
+        MemberEntity member;
+        OfferingEntity offering;
+
+        @BeforeEach
+        void setUp() {
+            member = memberFixture.createMember("dora");
+            offering = offeringFixture.createOffering(member);
+            offeringMemberFixture.createProposer(member, offering);
+        }
+
+        @DisplayName("로그인한 유저가 참여한 댓글방 목록을 조회할 수 있다")
+        @Test
+        void should_getAllCommentRoom_when_givenLoginMember() {
+            // when
+            CommentRoomAllResponse response = commentService.getAllCommentRoom(member);
+
+            // then
+            assertEquals(response.offerings().size(), 1);
+        }
+
+        @DisplayName("댓글방 목록 조회 시 삭제된 공모에 대한 댓글방은 제목에 삭제되었다고 명시되어 있다")
+        @Test
+        void should_getAllCommentRoomWithDeletedCommentRoom_when_giveLoginMember() {
+            // given
+            offeringFixture.deleteOffering(offering);
+
+            // when
+            CommentRoomAllResponse response = commentService.getAllCommentRoom(member);
+
+            // then
+            assertEquals(response.offerings().get(0).offeringTitle(), "삭제된 공동구매입니다.");
+        }
+    }
+
+    @DisplayName("댓글방 정보 조회")
+    @Nested
+    class GetCommentRoomInfo {
+
+        MemberEntity member;
+        OfferingEntity offering;
+
+        @BeforeEach
+        void setUp() {
+            member = memberFixture.createMember("dora");
+            offering = offeringFixture.createOffering(member);
+            offeringMemberFixture.createProposer(member, offering);
+        }
+
+        @DisplayName("삭제되지 않은 공모 id를 통해 댓글방 상세 조회를 할 수 있다")
+        @Test
+        void should_getExistedCommentRoomInfo_when_givenOfferingId() {
+            // when
+            CommentRoomInfoResponse response = commentService.getCommentRoomInfo(offering.getId(), member);
+
+            // then
+            assertEquals(response.title(), offering.getTitle());
+        }
+
+        @DisplayName("삭제된 공모 id를 통해 삭제된 공모라고 명시된 댓글방 상세 조회를 할 수 있다")
+        @Test
+        void should_getDeletedCommentRoomInfo_when_givenDeletedOfferingId() {
+            // given
+            offeringFixture.deleteOffering(offering);
+
+            // when
+            CommentRoomInfoResponse response = commentService.getCommentRoomInfo(offering.getId(), member);
+
+            // then
+            assertEquals(response.status(), CommentRoomStatus.DELETED);
+            assertEquals(response.title(), "삭제된 공동구매입니다.");
+        }
+    }
+}
