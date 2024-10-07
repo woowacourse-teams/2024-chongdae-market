af885d9d3e870fbbf6abcda3d3e459887d58ad6e
SCY <helenason@naver.com>
2024-10-07 16:12:19 +0900

    test: 닉네임 생성 파일 변경 후 테스트 수정 (#524)

--- backend/src/test/java/com/zzang/chongdae/member/service/NicknameGeneratorTest.java
+++ backend/src/test/java/com/zzang/chongdae/member/service/NicknameGeneratorTest.java
@@ -18,7 +18,7 @@ public class NicknameGeneratorTest {
     @Test
     void should_returnNickname_when_generateNickName() {
         // given
-        String expected = "춤추는도라";
+        String expected = "춤추는장성";
 
         // when
         String actual = nickNameGenerator.generate();
