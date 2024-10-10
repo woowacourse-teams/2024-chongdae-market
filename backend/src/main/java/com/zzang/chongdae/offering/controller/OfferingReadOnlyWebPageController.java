f0423a765b95bef952a273049afb80dcfbfc9e26
fromitive <46563149+fromitive@users.noreply.github.com>
2024-10-10 14:20:32 +0900

    feat: 총대마켓 읽기 전용 web 페이지 구축 (#528)
    * test: 읽기 전용 API 분리 및 문서화 적용
    
    * refactor: test에서 사용하지 않은 필드를 setUp으로 이동
    
    * feat: 총대마켓 상세 페이지 기능 구현
    
    * feat: og image url 변경
    
    * refactor: 공'고' -> '모' 로 수정
    
    * feat: 공모 목록 웹페이지 구현
    
    * refactor: 공모 마감 문구 변경
    
    * fix: 필터 변경시 검색어가 초기화 되는 문제 해결
    
    * style: 주석 제거
    
    * feat: 검색 및 필터창 고정하도록 변경
    
    * feat: 타이틀 감성 한 스푼
    
    * feat: 목록 스크롤 시 어색해 보이는 부분 개선

---
+++ backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingReadOnlyWebPageController.java
@@ -0,0 +1,19 @@
+package com.zzang.chongdae.offering.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class OfferingReadOnlyWebPageController {
+
+    @GetMapping("/read-only/web/offerings/{offering-id}")
+    String renderOfferingDetail(@PathVariable("offering-id") Long offeringId) {
+        return "detail";
+    }
+
+    @GetMapping("/read-only/web/offerings")
+    String renderOffering() {
+        return "index";
+    }
+}
