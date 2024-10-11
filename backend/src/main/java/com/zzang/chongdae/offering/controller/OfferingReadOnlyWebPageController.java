b920286f6f4625373edd32b6c5b1d7cf35c25bdd
fromitive <46563149+fromitive@users.noreply.github.com>
2024-10-11 20:29:18 +0900

    feat: 웹 접근성 개선 (#575)
    * feat: 파비콘 추가 :)
    
    * feat: 공유 시 메타 정보가 보이도록 변경

--- backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingReadOnlyWebPageController.java
+++ backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingReadOnlyWebPageController.java
@@ -1,14 +1,24 @@
 package com.zzang.chongdae.offering.controller;
 
+import com.zzang.chongdae.offering.service.OfferingService;
+import com.zzang.chongdae.offering.service.dto.OfferingMetaResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class OfferingReadOnlyWebPageController {
 
+    private final OfferingService offeringService;
+
     @GetMapping("/read-only/web/offerings/{offering-id}")
-    String renderOfferingDetail(@PathVariable("offering-id") Long offeringId) {
+    String renderOfferingDetail(@PathVariable("offering-id") Long offeringId, Model model) {
+        OfferingMetaResponse response = offeringService.getOfferingMetaInfo(offeringId);
+        model.addAttribute("offeringTitle", response.title());
+        model.addAttribute("thumbnailUrl", response.thumbnailUrl());
         return "detail";
     }
 
