9a26c3aa797981ed88c0fa2b2ed98e69396d4633
채현 <wjdcogus202@sookmyung.ac.kr>
2024-10-11 11:35:29 +0900

    test: test fix (#555)

--- android/app/src/test/java/com/zzang/chongdae/repository/FakeOfferingDetailRepository.kt
+++ android/app/src/test/java/com/zzang/chongdae/repository/FakeOfferingDetailRepository.kt
@@ -6,12 +6,16 @@ import com.zzang.chongdae.domain.model.OfferingDetail
 import com.zzang.chongdae.domain.repository.OfferingDetailRepository
 import com.zzang.chongdae.util.TestFixture
 
-class FakeOfferingDetailRepository : OfferingDetailRepository {
+class FakeOfferingDetailRepository() : OfferingDetailRepository {
     override suspend fun fetchOfferingDetail(offeringId: Long): Result<OfferingDetail, DataError.Network> {
         return Result.Success(TestFixture.OFFERING_DETAIL_STUB)
     }
 
     override suspend fun saveParticipation(offeringId: Long): Result<Unit, DataError.Network> {
         return Result.Success(Unit)
+    }
+
+    override suspend fun deleteOffering(offeringId: Long): Result<Unit, DataError.Network> {
+        TODO("Not yet implemented")
     }
 }
