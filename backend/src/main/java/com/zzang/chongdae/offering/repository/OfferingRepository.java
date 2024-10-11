66c46c85d1b9c1778545d163992550e22d96c088
masonkimseoul <masonkimseoul@gmail.com>
2024-10-11 13:07:47 +0900

    style: 불필요한 import 삭제

--- backend/src/main/java/com/zzang/chongdae/offering/repository/OfferingRepository.java
+++ backend/src/main/java/com/zzang/chongdae/offering/repository/OfferingRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface OfferingRepository extends JpaRepository<OfferingEntity, Long> {
 
