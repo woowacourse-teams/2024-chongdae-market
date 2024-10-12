5c3e8094564a1e29c749816ed9f7efec454646a6
SCY <helenason@naver.com>
2024-10-12 20:03:38 +0900

    refactor: 참여가능만 공모 조회 쿼리 최적화

--- backend/src/main/java/com/zzang/chongdae/offering/domain/offeringfetchstrategy/JoinableOfferingStrategy.java
+++ backend/src/main/java/com/zzang/chongdae/offering/domain/offeringfetchstrategy/JoinableOfferingStrategy.java
@@ -2,7 +2,6 @@ package com.zzang.chongdae.offering.domain.offeringfetchstrategy;
 
 import com.zzang.chongdae.offering.repository.OfferingRepository;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 
@@ -29,16 +28,6 @@ public class JoinableOfferingStrategy extends OfferingFetchStrategy {
         if (searchKeyword == null) {
             return offeringRepository.findJoinableOfferingsWithoutKeyword(outOfRangeId, pageable);
         }
-        return of(offeringRepository.findJoinableOfferingsWithTitleKeyword(outOfRangeId, searchKeyword, pageable),
-                offeringRepository.findJoinableOfferingsWithMeetingAddressKeyword(outOfRangeId, searchKeyword,
-                        pageable));
-    }
-
-    private List<OfferingEntity> of(List<OfferingEntity>... offerings) {
-        List<OfferingEntity> result = new ArrayList<>();
-        for (List<OfferingEntity> offering : offerings) {
-            result.addAll(offering);
-        } // todo: 정렬 로직 추가
-        return result;
+        return offeringRepository.findJoinableOfferingsWithTitleKeyword(outOfRangeId, searchKeyword, pageable);
     }
 }
--- backend/src/main/java/com/zzang/chongdae/offering/repository/OfferingRepository.java
+++ backend/src/main/java/com/zzang/chongdae/offering/repository/OfferingRepository.java
@@ -105,16 +105,6 @@ public interface OfferingRepository extends JpaRepository<OfferingEntity, Long> 
             double lastDiscountRate, Long lastId, String keyword, Pageable pageable);
 
     // ============================================================
-//    @Query("""
-//            SELECT o
-//            FROM OfferingEntity o
-//            WHERE (o.offeringStatus IN ('AVAILABLE', 'IMMINENT'))
-//               AND (o.id < :lastId)
-//               AND (:keyword IS NULL OR o.title LIKE :keyword% OR o.meetingAddress LIKE :keyword%)
-//            ORDER BY o.id DESC
-//            """)
-//    List<OfferingEntity> findJoinableOfferingsWithKeyword(Long lastId, String keyword, Pageable pageable);
-
     @Query("""
             SELECT o
             FROM OfferingEntity o
@@ -128,21 +118,12 @@ public interface OfferingRepository extends JpaRepository<OfferingEntity, Long> 
             SELECT o
             FROM OfferingEntity o
             WHERE (o.id < :lastId)
-               AND (o.title LIKE :keyword%)
+               AND (o.title LIKE :keyword% OR o.meetingAddress LIKE :keyword%)
                AND (o.offeringStatus IN ('AVAILABLE', 'IMMINENT'))
             ORDER BY o.id DESC
             """)
     List<OfferingEntity> findJoinableOfferingsWithTitleKeyword(Long lastId, String keyword, Pageable pageable);
 
-    @Query("""
-            SELECT o
-            FROM OfferingEntity o
-            WHERE (o.id < :lastId)
-               AND (o.meetingAddress LIKE :keyword%)
-               AND (o.offeringStatus IN ('AVAILABLE', 'IMMINENT'))
-            ORDER BY o.id DESC
-            """)
-    List<OfferingEntity> findJoinableOfferingsWithMeetingAddressKeyword(Long lastId, String keyword, Pageable pageable);
     // ============================================================
 
     @Query("SELECT MAX(o.id) FROM OfferingEntity o")
