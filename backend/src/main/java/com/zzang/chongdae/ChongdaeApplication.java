d8314e6d5e49d943bb4a4f3b48a6a67db55c23db
songpink <jejeje04@naver.com>
2024-10-14 15:20:36 +0900

    chore: CICD 테스트

--- backend/src/main/java/com/zzang/chongdae/ChongdaeApplication.java
+++ backend/src/main/java/com/zzang/chongdae/ChongdaeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChongdaeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChongdaeApplication.class, args);
     }
