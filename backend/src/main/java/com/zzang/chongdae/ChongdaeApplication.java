7d6006f6fc58f19100937983b0adfe94239087b3
songpink <jejeje04@naver.com>
2024-10-16 11:33:54 +0900

    chore: 백엔드 코드 수정

--- backend/src/main/java/com/zzang/chongdae/ChongdaeApplication.java
+++ backend/src/main/java/com/zzang/chongdae/ChongdaeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChongdaeApplication {
-    // 백엔드 코드 수정
+
     public static void main(String[] args) {
         SpringApplication.run(ChongdaeApplication.class, args);
     }
