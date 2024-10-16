0101104ff170ddcc4ad42da6cebe3ed94f5d011a
songpink <jejeje04@naver.com>
2024-10-16 11:39:52 +0900

    chore: push 트리거 테스트 원복

--- android/app/src/main/java/com/zzang/chongdae/ChongdaeApp.kt
+++ android/app/src/main/java/com/zzang/chongdae/ChongdaeApp.kt
@@ -10,7 +10,6 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ChongdaeApp : Application() {
-    // 안드 코드 수정
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
