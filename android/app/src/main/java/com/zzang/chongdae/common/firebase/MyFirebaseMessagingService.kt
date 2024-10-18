a310670069c2c94f48391e0d8ee41c872bf8950c
songpink <jejeje04@naver.com>
2024-10-18 19:03:01 +0900

    refactor: 함수 분리

--- android/app/src/main/java/com/zzang/chongdae/common/firebase/MyFirebaseMessagingService.kt
+++ android/app/src/main/java/com/zzang/chongdae/common/firebase/MyFirebaseMessagingService.kt
@@ -5,11 +5,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import com.zzang.chongdae.R
 import com.zzang.chongdae.presentation.view.MainActivity
 
@@ -24,7 +23,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     private fun sendNotification(title: String?, messageBody: String?) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 1001, intent, PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 1001, intent, PendingIntent.FLAG_IMMUTABLE)
 
         val channelId = "default_channel"
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
@@ -35,16 +35,21 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setContentIntent(pendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Default Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
+        createNotificationChannel(channelId, notificationManager)
 
         notificationManager.notify(1001, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel(
+        channelId: String,
+        notificationManager: NotificationManager
+    ) {
+        val channel = NotificationChannel(
+            channelId,
+            "Default Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 
     override fun onNewToken(token: String) {
