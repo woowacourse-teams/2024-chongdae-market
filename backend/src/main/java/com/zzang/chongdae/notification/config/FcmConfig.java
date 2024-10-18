d52123ccf14e32b609dbb14b4e443e7709ab1eef
SCY <helenason@naver.com>
2024-10-18 22:55:49 +0900

    chore: fcm key 빈 파일 생성

--- backend/src/main/java/com/zzang/chongdae/notification/config/FcmConfig.java
+++ backend/src/main/java/com/zzang/chongdae/notification/config/FcmConfig.java
@@ -29,7 +29,7 @@ public class FcmConfig {
             return;
         }
         try {
-            URL url = this.getClass().getResource("/" + secretKeyPath); // todo: path 정리
+            URL url = this.getClass().getResource(secretKeyPath);
             if (url == null) {
                 throw new MarketException(NotificationErrorCode.CANNOT_FIND_URL);
             }
