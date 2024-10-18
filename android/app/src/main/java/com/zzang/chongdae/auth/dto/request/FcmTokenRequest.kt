03a25f75ee012960452f3c80289e9d4e72de6a0c
songpink <jejeje04@naver.com>
2024-10-18 21:23:37 +0900

    refactor: 필요 없어진 파일 제거

--- android/app/src/main/java/com/zzang/chongdae/auth/dto/request/FcmTokenRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.zzang.chongdae.auth.dto.request
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class FcmTokenRequest(
-    @SerialName("fcmToken") val fcmToken: String,
-)
