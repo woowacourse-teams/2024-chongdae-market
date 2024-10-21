74dffde4864dcda842bdf8244e0bf0dd6402a323
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-10-21 15:33:09 +0900

    test: 로직 변경에 따른 test 코드 수정

--- android/app/src/test/java/com/zzang/chongdae/ExampleUnitTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.zzang.chongdae
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
