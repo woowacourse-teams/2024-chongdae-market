3b7bf24c5e6d5a15d0cb4423db3ed34ec933e84e
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-10-21 15:32:48 +0900

    feat: 변경에 따른 뷰 반영

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/adapter/comment/DateSeparatorViewHolder.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/adapter/comment/DateSeparatorViewHolder.kt
@@ -2,12 +2,12 @@ package com.zzang.chongdae.presentation.view.commentdetail.adapter.comment
 
 import androidx.recyclerview.widget.RecyclerView
 import com.zzang.chongdae.databinding.ItemDateSeparatorBinding
-import com.zzang.chongdae.domain.model.Comment
+import com.zzang.chongdae.presentation.view.commentdetail.model.comment.CommentUiModel
 
 class DateSeparatorViewHolder(
     private val binding: ItemDateSeparatorBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(comment: Comment) {
+    fun bind(comment: CommentUiModel) {
         binding.comment = comment
     }
 }
