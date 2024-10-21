0366b0f9031708f774dc18f9dd0403e44b9969d1
SCY <helenason@naver.com>
2024-10-21 17:53:01 +0900

    refactor: 거래 상태 알림 문구 변경

--- backend/src/main/java/com/zzang/chongdae/notification/domain/RoomStatusNotification.java
+++ backend/src/main/java/com/zzang/chongdae/notification/domain/RoomStatusNotification.java
@@ -1,8 +1,12 @@
 package com.zzang.chongdae.notification.domain;
 
 import com.google.firebase.messaging.Message;
+import com.zzang.chongdae.global.exception.MarketException;
+import com.zzang.chongdae.notification.exception.NotificationErrorCode;
 import com.zzang.chongdae.notification.service.FcmMessageManager;
+import com.zzang.chongdae.offering.domain.CommentRoomStatus;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +17,7 @@ public class RoomStatusNotification {
     public static final String TOPIC_FORMAT_OFFERING = "offering_%d"; // todo: topic 도메인 추출
     public static final String TOPIC_FORMAT_OFFERING_PROPOSER = "proposer_%d";
 
-    private static final String CONDITION_FORMAT = "'%s' in topics && !('%s' in topics)"; // todo: 부정문 가능한지 안드 테스트
+    private static final String CONDITION_FORMAT = "'%s' in topics && !('%s' in topics)";
 
     private final FcmMessageManager messageManager;
     private final OfferingEntity offering;
@@ -22,7 +26,7 @@ public class RoomStatusNotification {
         FcmCondition condition = createCondition();
         FcmData data = new FcmData();
         data.addData("title", offering.getTitle());
-        data.addData("body", "거래 상태가 %s로 변경되었습니다.".formatted(offering.getRoomStatus()));
+        data.addData("body", CommentRoomStatusMapper.getView(offering.getRoomStatus()));
         data.addData("offering_id", offering.getId());
         return messageManager.createMessage(condition, data);
     }
@@ -33,5 +37,31 @@ public class RoomStatusNotification {
         String condition = CONDITION_FORMAT.formatted(offeringTopic, isProposerTopic);
         log.info("[{}] 조건에 보내는 메시지", condition);
         return new FcmCondition(condition);
+    }
+
+    enum CommentRoomStatusMapper {
+
+        DELETED(CommentRoomStatus.DELETED, ""),
+        GROUPING(CommentRoomStatus.GROUPING, ""),
+        BUYING(CommentRoomStatus.BUYING, "모집이 마감됐어요! 이제 총대가 물건을 구매할 거예요"),
+        TRADING(CommentRoomStatus.TRADING, "총대가 물건을 구매했어요! 이제 거래를 진행해보아요"),
+        DONE(CommentRoomStatus.DONE, "거래가 완료되었어요. 고마워요 :)"),
+        ;
+
+        private CommentRoomStatus status;
+        private String view;
+
+        CommentRoomStatusMapper(CommentRoomStatus status, String view) {
+            this.status = status;
+            this.view = view;
+        }
+
+        public static String getView(CommentRoomStatus roomStatus) {
+            return Arrays.stream(values())
+                    .filter(v -> v.status.equals(roomStatus))
+                    .findAny()
+                    .orElseThrow(() -> new MarketException(NotificationErrorCode.INVALID_COMMENT_ROOM_STATUS))
+                    .view;
+        }
     }
 }
