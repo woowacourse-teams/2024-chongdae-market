c512f4fc18311200cf67fecf09069533e27f4b86
SCY <helenason@naver.com>
2024-10-21 19:53:05 +0900

    refactor: FcmTopic 구현

---
+++ backend/src/main/java/com/zzang/chongdae/notification/domain/FcmTopic.java
@@ -0,0 +1,11 @@
+package com.zzang.chongdae.notification.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class FcmTopic {
+
+    private final String value;
+}
