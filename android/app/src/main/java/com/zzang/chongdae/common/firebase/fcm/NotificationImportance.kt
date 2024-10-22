214e0c7238382f81676b8d1c4a150b69309570cb
songpink <jejeje04@naver.com>
2024-10-22 16:01:25 +0900

    refactor: 문자열 상수화

--- android/app/src/main/java/com/zzang/chongdae/common/firebase/fcm/NotificationImportance.kt
+++ android/app/src/main/java/com/zzang/chongdae/common/firebase/fcm/NotificationImportance.kt
@@ -9,13 +9,20 @@ sealed interface NotificationImportance {
 
     data object Default : NotificationImportance {
         override val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
-        override val channelId: String = "channel_importance_default"
-        override val channelName: String = "Default Channel"
+        override val channelId: String = CHANNEL_ID_IMPORTANCE_DEFAULT
+        override val channelName: String = CHANNEL_NAME_IMPORTANCE_DEFAULT
     }
 
     data object High : NotificationImportance {
         override val importance: Int = NotificationManager.IMPORTANCE_HIGH
-        override val channelId: String = "channel_importance_high"
-        override val channelName: String = "High Channel"
+        override val channelId: String = CHANNEL_ID_IMPORTANCE_HIGH
+        override val channelName: String = CHANNEL_NAME_IMPORTANCE_HIGH
+    }
+
+    companion object {
+        private const val CHANNEL_ID_IMPORTANCE_DEFAULT = "channel_importance_default"
+        private const val CHANNEL_NAME_IMPORTANCE_DEFAULT = "Default Channel"
+        private const val CHANNEL_ID_IMPORTANCE_HIGH = "channel_importance_high"
+        private const val CHANNEL_NAME_IMPORTANCE_HIGH = "High Channel"
     }
 }
