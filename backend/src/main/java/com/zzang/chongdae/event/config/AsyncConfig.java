fb6492c9535fca3143e9a39c090c3fbb09ffcc40
SCY <helenason@naver.com>
2024-10-23 22:01:33 +0900

    feat: 비동기 처리 config 구현

---
+++ backend/src/main/java/com/zzang/chongdae/event/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.zzang.chongdae.event.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(processors);
+        executor.setMaxPoolSize(processors * 2);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
