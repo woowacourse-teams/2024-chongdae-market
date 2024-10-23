90edf362a59af0e5f9d74a75f93d12650bc52fd1
SCY <helenason@naver.com>
2024-10-23 15:41:00 +0900

    test: 리팩터링 변경 사항 반영

--- backend/src/test/java/com/zzang/chongdae/notification/domain/RoomStatusMessageManagerTest.java
+++ backend/src/test/java/com/zzang/chongdae/notification/domain/RoomStatusMessageManagerTest.java
@@ -5,12 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.firebase.messaging.Message;
 import com.zzang.chongdae.global.service.ServiceTest;
 import com.zzang.chongdae.member.repository.entity.MemberEntity;
+import com.zzang.chongdae.notification.service.message.FcmMessageCreator;
 import com.zzang.chongdae.notification.service.message.RoomStatusMessageManager;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class RoomStatusMessageManagerTest extends ServiceTest {
+
+    @Autowired
+    private FcmMessageCreator messageCreator;
 
     @DisplayName("FCM에 전송할 메시지를 생성한다.")
     @Test
@@ -18,7 +23,7 @@ class RoomStatusMessageManagerTest extends ServiceTest {
         // given
         MemberEntity proposer = memberFixture.createMember("ever");
         OfferingEntity offering = offeringFixture.createOffering(proposer);
-        RoomStatusMessageManager notification = new RoomStatusMessageManager();
+        RoomStatusMessageManager notification = new RoomStatusMessageManager(messageCreator);
 
         // when
         Message message = notification.messageWhenUpdateStatus(offering);
