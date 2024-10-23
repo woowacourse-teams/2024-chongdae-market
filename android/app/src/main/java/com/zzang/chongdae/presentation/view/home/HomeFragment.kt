9977898d97329deac967145ef09edfb29ea5d4ef
Namyunsuk <yunsuk0328@naver.com>
2024-10-23 14:53:20 +0900

    fix: 공모 참여 후 홈화면에서 상태가 바뀌지 않는 오류 수정

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/home/HomeFragment.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/home/HomeFragment.kt
@@ -118,11 +118,13 @@ class HomeFragment : Fragment(), OnOfferingClickListener {
 
     private fun initFragmentResultListener() {
         setFragmentResultListener(OfferingDetailFragment.OFFERING_DETAIL_BUNDLE_KEY) { _, bundle ->
-            viewModel.fetchUpdatedOffering(bundle.getLong(OfferingDetailFragment.UPDATED_OFFERING_ID_KEY))
-        }
+            if (bundle.containsKey(OfferingDetailFragment.UPDATED_OFFERING_ID_KEY)) {
+                viewModel.fetchUpdatedOffering(bundle.getLong(OfferingDetailFragment.UPDATED_OFFERING_ID_KEY))
+            }
 
-        setFragmentResultListener(OfferingDetailFragment.OFFERING_DETAIL_BUNDLE_KEY) { _, bundle ->
-            viewModel.refreshOfferings(bundle.getBoolean(OfferingDetailFragment.DELETED_OFFERING_ID_KEY))
+            if (bundle.containsKey(OfferingDetailFragment.DELETED_OFFERING_ID_KEY)) {
+                viewModel.refreshOfferings(bundle.getBoolean(OfferingDetailFragment.DELETED_OFFERING_ID_KEY))
+            }
         }
 
         setFragmentResultListener(OfferingWriteOptionalFragment.OFFERING_WRITE_BUNDLE_KEY) { _, bundle ->
@@ -221,7 +223,8 @@ class HomeFragment : Fragment(), OnOfferingClickListener {
         viewModel.updatedOffering.observe(viewLifecycleOwner) {
             offeringAdapter.addUpdatedItem(it.toList())
         }
-        viewModel.updatedOffering.getValue()?.toList()?.let { offeringAdapter.addUpdatedItem(it) }
+        viewModel.updatedOffering.getValue()?.toList()
+            ?.let { offeringAdapter.addUpdatedItem(it) }
 
         viewModel.refreshTokenExpiredEvent.observe(viewLifecycleOwner) {
             LoginActivity.startActivity(requireContext())
