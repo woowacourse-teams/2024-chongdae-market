aa4d750d577b0531b24cfafc415f4639cb645685
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2024-10-25 00:01:07 +0900

    feat: HikariCP, Tomcat 튜닝 (#644)
    * refactor: 임시로 DEV CI/CD 스크립트 수정
    
    * refactor: branch 경로 수정 및 docker에만 push 할 수 있도록 수정
    
    * refactor: accept-count 200
    
    * refactor: - accept-count 200, + hikari max pool size 50
    
    * refactor: + hikari max pool size 200
    
    * refactor: - hikari max pool size 30
    
    * refactor: OfferingEntity의 Member에 FetchType.LAZY 설정 및 트랜잭션 추가
    
    * refactor: getAllOffering에 트렌젝션 적용
    
    * chore: actuator에 hikariCP 메트릭 지표 추가
    
    * refactor: - hikari max pool size 20
    
    * refactor: + hikari max pool size 40
    
    * refactor: - hikari max pool size 30
    
    * refactor: - hikari max pool size 20
    
    * refactor: + hikari max pool size 50
    
    * refactor: - hikari max pool size 4
    
    * refactor: + hikari max pool size 40
    
    * refactor: hikari max pool size default(10)
    
    * refactor: + hikari max pool size 8
    
    * refactor: 테스트를 위해 공모 생성 시 알림 off
    
    * refactor: - tomcat max threads 3
    
    * refactor: + tomcat max threads 100
    
    * refactor: default로 회귀
    
    * feat: HikariCP, Tomcat 설정 및 주석 해제
    
    * refactor: metric 관련 설정 제거
    
    ---------
    
    Co-authored-by: fromitive <fromitive@gmail.com>
    Co-authored-by: Choo <choo000407@naver.com>

--- backend/src/main/java/com/zzang/chongdae/offering/repository/entity/OfferingEntity.java
+++ backend/src/main/java/com/zzang/chongdae/offering/repository/entity/OfferingEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +49,7 @@ public class OfferingEntity extends BaseTimeEntity {
     private Long id;
 
     @NotNull
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private MemberEntity member;
 
     @NotNull
