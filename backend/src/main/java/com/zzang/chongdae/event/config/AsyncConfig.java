513b2108717270d8ff06cb4f35ffa9acca49c9dc
SCY <helenason@naver.com>
2024-11-22 20:07:13 +0900

    feat: 비동기 로직 예외 처리

--- backend/src/main/java/com/zzang/chongdae/event/config/AsyncConfig.java
+++ backend/src/main/java/com/zzang/chongdae/event/config/AsyncConfig.java
@@ -1,16 +1,19 @@
 package com.zzang.chongdae.event.config;
 
-import org.springframework.context.annotation.Bean;
+import com.zzang.chongdae.event.exception.AsyncEventExceptionHandler;
+import java.util.concurrent.Executor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
-    @Bean
-    public ThreadPoolTaskExecutor taskExecutor() {
+    @Override
+    public Executor getAsyncExecutor() {
         int processors = Runtime.getRuntime().availableProcessors();
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(processors);
@@ -19,5 +22,10 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("AsyncExecutor-");
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncEventExceptionHandler();
     }
 }
