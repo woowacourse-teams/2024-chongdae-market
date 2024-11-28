7854bf9999732ff000bb65865308682ad29e0a49
fromitive <fromitive@gmail.com>
2024-11-28 20:59:48 +0900

    refactor: 이미지 업로드 메서드 시그니처 번경

--- android/app/src/test/java/com/zzang/chongdae/presentation/view/comment/CommentRoomsViewModelTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/presentation/view/comment/CommentRoomsViewModelTest.kt
@@ -1,12 +1,12 @@
 package com.zzang.chongdae.presentation.view.comment
 
-import com.zzang.chongdae.auth.repository.AuthRepository
 import com.zzang.chongdae.domain.repository.CommentRoomsRepository
-import com.zzang.chongdae.repository.FakeAuthRepository
-import com.zzang.chongdae.repository.FakeCommentRoomsRepository
 import com.zzang.chongdae.util.CoroutinesTestExtension
 import com.zzang.chongdae.util.InstantTaskExecutorExtension
+import com.zzang.chongdae.util.TestFixture
 import com.zzang.chongdae.util.getOrAwaitValue
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -20,19 +20,22 @@ import java.time.LocalDateTime
 @ExtendWith(InstantTaskExecutorExtension::class)
 class CommentRoomsViewModelTest {
     private lateinit var viewModel: CommentRoomsViewModel
-    private lateinit var authRepository: AuthRepository
-    private lateinit var commentRoomsRepository: CommentRoomsRepository
+    private lateinit var offeringRepository: CommentRoomsRepository
 
     @BeforeEach
     fun setUp() {
-        authRepository = FakeAuthRepository()
-        commentRoomsRepository = FakeCommentRoomsRepository()
-        viewModel = CommentRoomsViewModel(authRepository, commentRoomsRepository)
+        offeringRepository = mockk<CommentRoomsRepository>()
+        viewModel = CommentRoomsViewModel(offeringRepository)
     }
 
     @DisplayName("댓글방 목록을 확인할 수 있어야 한다")
     @Test
     fun loadCommentRooms() {
+        // given
+        coEvery {
+            offeringRepository.fetchCommentRooms(1).getOrThrow()
+        } returns TestFixture.COMMENT_ROOMS_STUB
+
         // when
         viewModel.updateCommentRooms()
 
