eeb79161423480281a2ed9277c1c64c54c9b8679
fromitive <fromitive@gmail.com>
2024-11-28 21:08:31 +0900

    refactor: 이미지 업로드 메서드 시그니처 번경

--- backend/src/main/java/com/zzang/chongdae/offering/util/httpclient/HtmlCrawler.java
+++
@@ -1,8 +0,0 @@
-package com.zzang.chongdae.offering.util.httpclient;
-
-public interface HtmlCrawler {
-
-    String scrap(String productUrl);
-
-    String parseOpenGraph(String html, String property);
-}
