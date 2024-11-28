760946ff9d00087af5e7ee33ec244c36a00e5dc2
fromitive <fromitive@gmail.com>
2024-11-28 20:59:31 +0900

    refactor: 이미지 업로드 메서드 시그니처 번경

--- android/app/src/test/java/com/zzang/chongdae/presentation/view/offeringdetail/OfferingDetailViewModelTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/presentation/view/offeringdetail/OfferingDetailViewModelTest.kt
@@ -1,15 +1,10 @@
 package com.zzang.chongdae.presentation.view.offeringdetail
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import com.zzang.chongdae.auth.repository.AuthRepository
-import com.zzang.chongdae.common.datastore.UserPreferencesDataStore
 import com.zzang.chongdae.domain.repository.OfferingDetailRepository
-import com.zzang.chongdae.repository.FakeAuthRepository
-import com.zzang.chongdae.repository.FakeDataStore
 import com.zzang.chongdae.repository.FakeOfferingDetailRepository
 import com.zzang.chongdae.util.CoroutinesTestExtension
 import com.zzang.chongdae.util.InstantTaskExecutorExtension
+import com.zzang.chongdae.util.TestFixture
 import com.zzang.chongdae.util.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.assertj.core.api.Assertions.assertThat
@@ -23,19 +18,26 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutorExtension::class)
 class OfferingDetailViewModelTest {
     private lateinit var viewModel: OfferingDetailViewModel
-    private lateinit var dataStore: DataStore<Preferences>
     private lateinit var offeringDetailRepository: OfferingDetailRepository
-    private lateinit var authRepository: AuthRepository
+
     private val offeringId = 1L
-    private lateinit var userPreferencesDataStore: UserPreferencesDataStore
 
     @BeforeEach
     fun setUp() {
-        dataStore = FakeDataStore()
         offeringDetailRepository = FakeOfferingDetailRepository()
-        authRepository = FakeAuthRepository()
-        userPreferencesDataStore = UserPreferencesDataStore(FakeDataStore())
-        viewModel = OfferingDetailViewModel(offeringId, offeringDetailRepository, authRepository, userPreferencesDataStore)
+        viewModel = OfferingDetailViewModel(offeringId, offeringDetailRepository)
+    }
+
+    @DisplayName("공모 상세정보를 불러온다")
+    @Test
+    fun loadOffering() {
+        // given
+
+        // when
+        val actual = viewModel.offeringDetail.getOrAwaitValue()
+
+        // then
+        assertThat(actual).isEqualTo(TestFixture.OFFERING_DETAIL_STUB)
     }
 
     @DisplayName("공구에 참여한다")
@@ -44,7 +46,7 @@ class OfferingDetailViewModelTest {
         // given
 
         // when
-        viewModel.participate()
+        viewModel.onClickParticipation()
         val actual = viewModel.isParticipated.getOrAwaitValue()
 
         // then
--- android/app/src/test/java/com/zzang/chongdae/repository/FakeOfferingDetailRepository.kt
+++ android/app/src/test/java/com/zzang/chongdae/repository/FakeOfferingDetailRepository.kt
@@ -1,21 +1,21 @@
 package com.zzang.chongdae.repository
 
-import com.zzang.chongdae.common.handler.DataError
-import com.zzang.chongdae.common.handler.Result
 import com.zzang.chongdae.domain.model.OfferingDetail
 import com.zzang.chongdae.domain.repository.OfferingDetailRepository
 import com.zzang.chongdae.util.TestFixture
 
-class FakeOfferingDetailRepository() : OfferingDetailRepository {
-    override suspend fun fetchOfferingDetail(offeringId: Long): Result<OfferingDetail, DataError.Network> {
-        return Result.Success(TestFixture.OFFERING_DETAIL_STUB)
+class FakeOfferingDetailRepository : OfferingDetailRepository {
+    override suspend fun fetchOfferingDetail(
+        memberId: Long,
+        offeringId: Long,
+    ): Result<OfferingDetail> {
+        return Result.success(TestFixture.OFFERING_DETAIL_STUB)
     }
 
-    override suspend fun saveParticipation(offeringId: Long): Result<Unit, DataError.Network> {
-        return Result.Success(Unit)
-    }
-
-    override suspend fun deleteOffering(offeringId: Long): Result<Unit, DataError.Network> {
-        TODO("Not yet implemented")
+    override suspend fun saveParticipation(
+        memberId: Long,
+        offeringId: Long,
+    ): Result<Unit> {
+        return Result.success(Unit)
     }
 }
