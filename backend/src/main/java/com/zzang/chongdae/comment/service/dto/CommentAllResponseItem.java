24e70735ddca6d782c4b1c9f78fc83cabf5d925d
fromitive <fromitive@gmail.com>
2024-11-28 21:05:35 +0900

    refactor: 이미지 업로드 메서드 시그니처 번경

--- backend/src/main/java/com/zzang/chongdae/comment/service/dto/CommentAllResponseItem.java
+++ backend/src/main/java/com/zzang/chongdae/comment/service/dto/CommentAllResponseItem.java
@@ -1,21 +1,8 @@
 package com.zzang.chongdae.comment.service.dto;
 
-import com.zzang.chongdae.comment.repository.entity.CommentEntity;
-import com.zzang.chongdae.member.repository.entity.MemberEntity;
-
-public record CommentAllResponseItem(Long commentId,
-                                     CommentCreatedAtResponse createdAt,
+public record CommentAllResponseItem(CommentCreatedAtResponse createdAt,
                                      String content,
                                      String nickname,
-                                     boolean isProposer,
-                                     boolean isMine) {
-
-    public CommentAllResponseItem(CommentEntity comment, MemberEntity member) {
-        this(comment.getId(),
-                new CommentCreatedAtResponse(comment.getCreatedAt()),
-                comment.getContent(),
-                comment.getMember().getNickname(),
-                comment.getOffering().isProposedBy(comment.getMember()),
-                comment.isOwnedBy(member));
-    }
+                                     Boolean isProposer,
+                                     Boolean isMine) {
 }
