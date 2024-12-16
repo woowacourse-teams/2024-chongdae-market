0c6e0d45eecdd4ce2f39b4fab7f40cf4e768ce39
songpink <jejeje04@naver.com>
2024-12-16 16:13:40 +0900

    test: 로그인 유즈케이스 테스트

--- android/app/src/test/java/com/zzang/chongdae/domain/usecase/login/PostLoginUseCaseTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/domain/usecase/login/PostLoginUseCaseTest.kt
@@ -1,37 +1,38 @@
-//package com.zzang.chongdae.domain.usecase.login
-//
-//import com.zzang.chongdae.auth.repository.AuthRepository
-//import com.zzang.chongdae.common.datastore.UserPreferencesDataStore
-//import com.zzang.chongdae.repository.FakeAuthRepository
-//import com.zzang.chongdae.repository.FakeDataStore
-//import com.zzang.chongdae.util.CoroutinesTestExtension
-//import kotlinx.coroutines.ExperimentalCoroutinesApi
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.extension.ExtendWith
-//
-//@ExperimentalCoroutinesApi
-//@ExtendWith(CoroutinesTestExtension::class)
-//class PostLoginUseCaseTest {
-//    private lateinit var authRepository: AuthRepository
-//    private lateinit var userPreferenceDataStore: UserPreferencesDataStore
-//    private lateinit var postLoginUseCase: PostLoginUseCase
-//
-//    @BeforeEach
-//    fun setUp() {
-//        authRepository = FakeAuthRepository()
-//        userPreferenceDataStore = UserPreferencesDataStore(FakeDataStore())
-//        postLoginUseCase = PostLoginUseCaseImpl(authRepository, userPreferenceDataStore)
-//    }
-//
-//    @Test
-//    fun `임시 이름`() {
-//        // given
-//        val a = postLoginUseCase("FakeAccessToken", "FakeRefreshToken")
-//
-//        // when
-//
-//        // then
-//
-//    }
-//}
+package com.zzang.chongdae.domain.usecase.login
+
+import com.zzang.chongdae.auth.repository.AuthRepository
+import com.zzang.chongdae.common.datastore.UserPreferencesDataStore
+import com.zzang.chongdae.common.handler.Result
+import com.zzang.chongdae.repository.FakeAuthRepository
+import com.zzang.chongdae.repository.FakeDataStore
+import com.zzang.chongdae.util.CoroutinesTestExtension
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+class PostLoginUseCaseTest {
+    private lateinit var authRepository: AuthRepository
+    private lateinit var userPreferenceDataStore: UserPreferencesDataStore
+    private lateinit var postLoginUseCase: PostLoginUseCase
+
+    @BeforeEach
+    fun setUp() {
+        authRepository = FakeAuthRepository()
+        userPreferenceDataStore = UserPreferencesDataStore(FakeDataStore())
+        postLoginUseCase = PostLoginUseCaseImpl(authRepository, userPreferenceDataStore)
+    }
+
+    @Test
+    fun `로그인에 성공한다`() = runTest {
+        // given
+
+        // when
+        val result = postLoginUseCase("FakeAccessToken", "FakeRefreshToken")
+
+        // then
+        assertThat(result).isInstanceOf(Result.Success::class.java)
+    }
+}
