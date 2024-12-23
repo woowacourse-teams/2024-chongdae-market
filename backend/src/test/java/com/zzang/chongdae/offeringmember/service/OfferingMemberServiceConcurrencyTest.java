d4976995e035e18775bd5d2d590092408343fe7e
SCY <helenason@naver.com>
2024-12-23 19:13:47 +0900

    test: 동시 실행 코드 ConcurrencyExecutor 클래스로 추출

--- backend/src/test/java/com/zzang/chongdae/offeringmember/service/OfferingMemberServiceConcurrencyTest.java
+++ backend/src/test/java/com/zzang/chongdae/offeringmember/service/OfferingMemberServiceConcurrencyTest.java
@@ -2,14 +2,12 @@ package com.zzang.chongdae.offeringmember.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.zzang.chongdae.global.helper.ConcurrencyExecutor;
 import com.zzang.chongdae.global.service.ServiceTest;
 import com.zzang.chongdae.member.repository.entity.MemberEntity;
 import com.zzang.chongdae.offering.repository.entity.OfferingEntity;
 import com.zzang.chongdae.offeringmember.service.dto.ParticipantResponse;
 import com.zzang.chongdae.offeringmember.service.dto.ParticipationRequest;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,22 +29,9 @@ class OfferingMemberServiceConcurrencyTest extends ServiceTest {
         ParticipationRequest request = new ParticipationRequest(offering.getId());
         MemberEntity participant = memberFixture.createMember("whoever");
 
-        int executeCount = 5;
-        ExecutorService executorService = Executors.newFixedThreadPool(executeCount);
-        CountDownLatch countDownLatch = new CountDownLatch(executeCount);
-
-        for (int i = 0; i < executeCount; i++) {
-            executorService.submit(() -> {
-                try {
-                    offeringMemberService.participate(request, participant);
-                } finally {
-                    countDownLatch.countDown();
-                }
-            });
-        }
-
-        countDownLatch.await();
-        executorService.shutdown();
+        ConcurrencyExecutor concurrencyExecutor = ConcurrencyExecutor.getInstance();
+        concurrencyExecutor.executeWithoutResult(5,
+                () -> offeringMemberService.participate(request, participant));
 
         // then
         ParticipantResponse response = offeringMemberService.getAllParticipant(offering.getId(), proposer);
@@ -66,27 +51,10 @@ class OfferingMemberServiceConcurrencyTest extends ServiceTest {
         MemberEntity participant1 = memberFixture.createMember("ever1");
         MemberEntity participant2 = memberFixture.createMember("ever2");
 
-        int executeCount = 2;
-        ExecutorService executorService = Executors.newFixedThreadPool(executeCount);
-        CountDownLatch countDownLatch = new CountDownLatch(executeCount);
-
-        executorService.submit(() -> {
-            try {
-                offeringMemberService.participate(request, participant1);
-            } finally {
-                countDownLatch.countDown();
-            }
-        });
-        executorService.submit(() -> {
-            try {
-                offeringMemberService.participate(request, participant2);
-            } finally {
-                countDownLatch.countDown();
-            }
-        });
-
-        countDownLatch.await();
-        executorService.shutdown();
+        ConcurrencyExecutor concurrencyExecutor = ConcurrencyExecutor.getInstance();
+        concurrencyExecutor.executeWithoutResult(
+                () -> offeringMemberService.participate(request, participant1),
+                () -> offeringMemberService.participate(request, participant2));
 
         // then
         ParticipantResponse response = offeringMemberService.getAllParticipant(offering.getId(), proposer);
