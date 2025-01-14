586c76e112e2b41f1a67adf28250f7deb87b1b9e
Namyunsuk <yunsuk0328@naver.com>
2025-01-15 00:28:09 +0900

    test: usecase적용에 따른 viewmodel 테스트 수정

--- android/app/src/test/java/com/zzang/chongdae/presentation/view/home/OfferingViewModelTest.kt
+++ android/app/src/test/java/com/zzang/chongdae/presentation/view/home/OfferingViewModelTest.kt
@@ -3,6 +3,9 @@ package com.zzang.chongdae.presentation.view.home
 import com.zzang.chongdae.auth.repository.AuthRepository
 import com.zzang.chongdae.common.datastore.UserPreferencesDataStore
 import com.zzang.chongdae.domain.repository.OfferingRepository
+import com.zzang.chongdae.domain.usecase.home.FetchFiltersUserCase
+import com.zzang.chongdae.domain.usecase.home.FetchOfferingUseCase
+import com.zzang.chongdae.domain.usecase.home.FetchOfferingsUseCase
 import com.zzang.chongdae.repository.FakeAuthRepository
 import com.zzang.chongdae.repository.FakeDataStore
 import com.zzang.chongdae.repository.FakeOfferingRepository
@@ -23,14 +26,26 @@ class OfferingViewModelTest {
     private lateinit var viewModel: OfferingViewModel
     private lateinit var offeringRepository: OfferingRepository
     private lateinit var authRepository: AuthRepository
+    private lateinit var fetchOfferingsUseCase: FetchOfferingsUseCase
+    private lateinit var fetchFiltersUserCase: FetchFiltersUserCase
+    private lateinit var fetchOfferingUseCase: FetchOfferingUseCase
     private lateinit var userPreferencesDataStore: UserPreferencesDataStore
 
     @BeforeEach
     fun setUp() {
         offeringRepository = FakeOfferingRepository()
         authRepository = FakeAuthRepository()
+        fetchOfferingsUseCase = FetchOfferingsUseCase(offeringRepository)
+        fetchFiltersUserCase = FetchFiltersUserCase(offeringRepository)
+        fetchOfferingUseCase = FetchOfferingUseCase(offeringRepository)
         userPreferencesDataStore = UserPreferencesDataStore(FakeDataStore())
-        viewModel = OfferingViewModel(offeringRepository, authRepository, userPreferencesDataStore)
+        viewModel = OfferingViewModel(
+            authRepository,
+            fetchOfferingsUseCase,
+            fetchFiltersUserCase,
+            fetchOfferingUseCase,
+            userPreferencesDataStore
+        )
     }
 
     @DisplayName("필터 정보를 불러온다")
