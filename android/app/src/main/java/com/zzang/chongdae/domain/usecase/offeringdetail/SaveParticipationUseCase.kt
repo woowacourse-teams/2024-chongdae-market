77e1e4f34c5eeb6cdaf08d8c200773f4040d6810
Namyunsuk <yunsuk0328@naver.com>
2025-01-15 00:49:31 +0900

    refactor: 공모 상세화면 usecase적용

---
+++ android/app/src/main/java/com/zzang/chongdae/domain/usecase/offeringdetail/SaveParticipationUseCase.kt
@@ -0,0 +1,12 @@
+package com.zzang.chongdae.domain.usecase.offeringdetail
+
+import com.zzang.chongdae.di.annotations.OfferingDetailRepositoryQualifier
+import com.zzang.chongdae.domain.repository.OfferingDetailRepository
+import javax.inject.Inject
+
+class SaveParticipationUseCase @Inject constructor(
+    @OfferingDetailRepositoryQualifier val offeringDetailRepository: OfferingDetailRepository,
+) {
+    suspend operator fun invoke(offeringId: Long) =
+        offeringDetailRepository.saveParticipation(offeringId)
+}
