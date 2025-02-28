e07abdd53092ef65be0e8c22bdd0d9db33d9cd6f
Namyunsuk <yunsuk0328@naver.com>
2025-02-28 21:01:59 +0900

    build: parcelize 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     kotlin("plugin.serialization") version "2.0.0"
     id("com.google.firebase.crashlytics")
     id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
 }
 
 android {
