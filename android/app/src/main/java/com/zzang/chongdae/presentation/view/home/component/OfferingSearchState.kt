90c681a9f43a6b023f99afaed31b5d1f0a45253f
Namyunsuk <yunsuk0328@naver.com>
2025-03-02 21:29:16 +0900

    refactor: 불필요한 파라미터 제거

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/home/component/OfferingSearchState.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/home/component/OfferingSearchState.kt
@@ -1,6 +1,5 @@
 package com.zzang.chongdae.presentation.view.home.component
 
-import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,19 +7,14 @@ import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import kotlinx.parcelize.Parcelize
 
 class OfferingSearchState(
     initialText: String = "",
     val placeholder: String? = null,
     val maxLength: Int? = null,
-    val validation: (String) -> Boolean = { true },
 ) {
     var text by mutableStateOf(initialText)
         private set
-
-    val isError: Boolean
-        get() = !validation(text)
 
     val isPlaceHolderVisible: Boolean
         get() = placeholder != null && text.isBlank()
@@ -42,7 +36,6 @@ class OfferingSearchState(
                     it.text,
                     it.placeholder,
                     it.maxLength,
-                    Validation(it.validation)
                 )
             },
             restore = {
@@ -50,29 +43,23 @@ class OfferingSearchState(
                     initialText = it[0] as String,
                     placeholder = it[2] as String?,
                     maxLength = it[3] as Int?,
-                    validation = (it[4] as Validation).validation,
                 )
             }
         )
     }
 }
 
-@Parcelize
-class Validation(val validation: (String) -> Boolean) : Parcelable
-
 @Composable
 fun rememberOfferingSearchState(
     initialText: String = "",
     placeholder: String? = null,
     maxLength: Int? = null,
-    validation: (String) -> Boolean = { true },
 ): OfferingSearchState {
     return rememberSaveable(saver = OfferingSearchState.Saver) {
         OfferingSearchState(
             initialText,
             placeholder,
             maxLength,
-            validation
         )
     }
 }
