49fa90c2325466dcca4a5067247b0a2cd069c8a3
fromitive <fromitive@gmail.com>
2025-03-03 07:21:10 +0900

    refactor: 오류 문구가 명확하게 전달되도록 변경

--- backend/src/main/java/com/zzang/chongdae/auth/exception/AuthErrorCode.java
+++ backend/src/main/java/com/zzang/chongdae/auth/exception/AuthErrorCode.java
@@ -19,7 +19,7 @@ public enum AuthErrorCode implements ErrorResponse {
     CLIENT_TIME_OUT(HttpStatus.INTERNAL_SERVER_ERROR, "시간이 초과되어 로그인 요청에 실패했습니다."),
     KAKAO_LOGIN_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오 로그인에 실패했습니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    REFRESH_REUSE_EXCEPTION(HttpStatus.UNAUTHORIZED, "이미 사용한 토큰입니다. 재 로그인이 필요합니다.");
+    REFRESH_REUSE_EXCEPTION(HttpStatus.UNAUTHORIZED, "이미 사용한 토큰입니다. 다시 로그인 해주세요.");
 
     private final HttpStatus status;
     private final String message;
