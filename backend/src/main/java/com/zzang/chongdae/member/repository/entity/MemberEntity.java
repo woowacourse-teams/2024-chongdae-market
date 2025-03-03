5e8f6423c0bd4f955ea397771a224ca368c1244a
fromitive <fromitive@gmail.com>
2025-03-04 05:50:56 +0900

    fix: import 추가

--- backend/src/main/java/com/zzang/chongdae/member/repository/entity/MemberEntity.java
+++ backend/src/main/java/com/zzang/chongdae/member/repository/entity/MemberEntity.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import lombok.AccessLevel;
