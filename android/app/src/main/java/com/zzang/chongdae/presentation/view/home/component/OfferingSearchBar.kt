44a5f36f6668667fbed156ba2d2c5d513aff63ab
Namyunsuk <yunsuk0328@naver.com>
2025-03-05 00:21:29 +0900

    refactor: 커서 색상 변경

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/home/component/OfferingSearchBar.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/home/component/OfferingSearchBar.kt
@@ -15,13 +15,17 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -45,102 +49,111 @@ internal fun OfferingSearchBar(
 ) {
     val density = LocalDensity.current
 
-    BasicTextField(
-        modifier =
-            modifier
-                .fillMaxWidth()
-                .background(Color.White),
-        value = state.text,
-        onValueChange = { state.updateText(it) },
-        textStyle =
-            TextStyle(
-                fontSize = with(density) { 15.dp.toSp() },
-                fontFamily =
-                    FontFamily(
-                        Font(
-                            R.font.suit_medium,
-                            weight = FontWeight.Medium,
+    val textSelectionColors =
+        TextSelectionColors(
+            handleColor = Transparent,
+            backgroundColor = colorResource(R.color.cursor_background_color),
+        )
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides textSelectionColors,
+    ) {
+        BasicTextField(
+            modifier =
+                modifier
+                    .fillMaxWidth()
+                    .background(Color.White),
+            value = state.text,
+            onValueChange = { state.updateText(it) },
+            textStyle =
+                TextStyle(
+                    fontSize = with(density) { 15.dp.toSp() },
+                    fontFamily =
+                        FontFamily(
+                            Font(
+                                R.font.suit_medium,
+                                weight = FontWeight.Medium,
+                            ),
                         ),
-                    ),
-            ),
-        keyboardOptions =
-            KeyboardOptions(
-                imeAction = ImeAction.Done,
-                keyboardType = KeyboardType.Text,
-            ),
-        keyboardActions =
-            KeyboardActions(
-                onDone = {
-                    onSearchClick()
-                },
-            ),
-    ) { innerTextField ->
-        Column {
-            Row(
-                modifier =
-                    Modifier
-                        .wrapContentHeight()
-                        .padding(start = 20.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
+                ),
+            keyboardOptions =
+                KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Text,
+                ),
+            keyboardActions =
+                KeyboardActions(
+                    onDone = {
+                        onSearchClick()
+                    },
+                ),
+        ) { innerTextField ->
+            Column {
+                Row(
+                    modifier =
+                        Modifier
+                            .wrapContentHeight()
+                            .padding(start = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Box(
+                        modifier =
+                            Modifier
+                                .weight(1f)
+                                .fillMaxWidth(),
+                    ) {
+                        innerTextField()
+                        if (state.isPlaceHolderVisible) {
+                            Text(
+                                text = state.placeholder ?: return@Box,
+                                maxLines = 1,
+                                style =
+                                    TextStyle(
+                                        fontSize = with(density) { 15.dp.toSp() },
+                                        color = Color.Gray,
+                                        fontFamily =
+                                            FontFamily(
+                                                Font(
+                                                    R.font.suit_medium,
+                                                    weight = FontWeight.Medium,
+                                                ),
+                                            ),
+                                    ),
+                            )
+                        }
+                    }
+
+                    if (state.text.isNotBlank()) {
+                        IconButton(
+                            modifier =
+                                Modifier
+                                    .size(15.dp),
+                            onClick = onClearClick,
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.btn_offering_write_clear),
+                                contentDescription = "Clear Search",
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(15.dp))
+                    }
+                    IconButton(
+                        modifier = Modifier.size(25.dp),
+                        onClick = onSearchClick,
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_main_search),
+                            contentDescription = "Search",
+                        )
+                    }
+                }
                 Box(
                     modifier =
                         Modifier
-                            .weight(1f)
-                            .fillMaxWidth(),
-                ) {
-                    innerTextField()
-                    if (state.isPlaceHolderVisible) {
-                        Text(
-                            text = state.placeholder ?: return@Box,
-                            maxLines = 1,
-                            style =
-                                TextStyle(
-                                    fontSize = with(density) { 15.dp.toSp() },
-                                    color = Color.Gray,
-                                    fontFamily =
-                                        FontFamily(
-                                            Font(
-                                                R.font.suit_medium,
-                                                weight = FontWeight.Medium,
-                                            ),
-                                        ),
-                                ),
-                        )
-                    }
-                }
-
-                if (state.text.isNotBlank()) {
-                    IconButton(
-                        modifier =
-                            Modifier
-                                .size(15.dp),
-                        onClick = onClearClick,
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.btn_offering_write_clear),
-                            contentDescription = "Clear Search",
-                        )
-                    }
-                    Spacer(modifier = Modifier.width(15.dp))
-                }
-                IconButton(
-                    modifier = Modifier.size(25.dp),
-                    onClick = onSearchClick,
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_main_search),
-                        contentDescription = "Search",
-                    )
-                }
+                            .fillMaxWidth()
+                            .height(1.5.dp)
+                            .background(color = colorResource(id = R.color.main_color)),
+                )
             }
-            Box(
-                modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .height(1.5.dp)
-                        .background(color = colorResource(id = R.color.main_color)),
-            )
         }
     }
 }
