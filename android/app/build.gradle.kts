7521379350c5c2f298c61bd9514ac228f0c6c70e
Namyunsuk <yunsuk0328@naver.com>
2025-03-06 02:23:00 +0900

    build: compose test 의존성 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -190,6 +190,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.ui.tooling)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
 
 kapt {
