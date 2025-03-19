b5e2bb2a6e904fde7dc0e48c08b1e1e60fad80ae
fromitive <fromitive@gmail.com>
2025-03-19 15:55:52 +0900

    style: 코드 컨밴션 적용

--- backend/src/main/java/com/zzang/chongdae/comment/domain/SearchDirection.java
+++ backend/src/main/java/com/zzang/chongdae/comment/domain/SearchDirection.java
@@ -1,15 +1,7 @@
 package com.zzang.chongdae.comment.domain;
 
-import java.util.Arrays;
-
 public enum SearchDirection {
+
     NEXT,
     PREVIOUS;
-
-    public static SearchDirection of(String direction) {
-        return Arrays.stream(SearchDirection.values())
-                .filter(searchDirection -> searchDirection.name().equals(direction))
-                .findFirst()
-                .orElseGet(() -> SearchDirection.PREVIOUS);
-    }
 }
