7881c0d62e2a8f314aaaa72a04f77e1416e76a3a
fromitive <fromitive@gmail.com>
2025-03-21 15:16:17 +0900

    fix: image url 프로파일 별로 설정하도록 수정

--- backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingReadOnlyWebPageController.java
+++ backend/src/main/java/com/zzang/chongdae/offering/controller/OfferingReadOnlyWebPageController.java
@@ -3,6 +3,7 @@ package com.zzang.chongdae.offering.controller;
 import com.zzang.chongdae.offering.service.OfferingService;
 import com.zzang.chongdae.offering.service.dto.OfferingMetaResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,21 @@ public class OfferingReadOnlyWebPageController {
 
     private final OfferingService offeringService;
 
+    @Value("${storage.resourceHost}")
+    private String resourceHost;
+
     @GetMapping("/read-only/web/offerings/{offering-id}")
     String renderOfferingDetail(@PathVariable("offering-id") Long offeringId, Model model) {
         OfferingMetaResponse response = offeringService.getOfferingMetaInfo(offeringId);
         model.addAttribute("offeringTitle", response.title());
         model.addAttribute("thumbnailUrl", response.thumbnailUrl());
+        model.addAttribute("resourceHost", resourceHost);
         return "detail";
     }
 
     @GetMapping("/read-only/web/offerings")
-    String renderOffering() {
+    String renderOffering(Model model) {
+        model.addAttribute("resourceHost", resourceHost);
         return "index";
     }
 }
