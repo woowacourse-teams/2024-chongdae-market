a459ec1e0380b6c22672a4ecc726774edff1dc01
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-03-24 17:20:36 +0900

    feat: 참여자 정보 ui model에 추가

--- android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/model/participants/ProposerUiModel.kt
+++ android/app/src/main/java/com/zzang/chongdae/presentation/view/commentdetail/model/participants/ProposerUiModel.kt
@@ -4,11 +4,15 @@ import com.zzang.chongdae.domain.model.participant.Proposer
 
 data class ProposerUiModel(
     val nickname: String,
+    val count: Int,
+    val price: Int,
 ) {
     companion object {
         fun Proposer.toUiModel(): ProposerUiModel {
             return ProposerUiModel(
                 nickname = nickname,
+                count = count,
+                price = price,
             )
         }
     }
