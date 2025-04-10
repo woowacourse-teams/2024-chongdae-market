7c88509741e04fc1a41f89da3f4b1a5e6911c68e
SCY <helenason@naver.com>
2025-04-10 19:39:00 +0900

    test: 재시도 테스트 예외 검증

--- backend/src/test/java/com/zzang/chongdae/notification/service/RetryableTest.java
+++ backend/src/test/java/com/zzang/chongdae/notification/service/RetryableTest.java
@@ -1,6 +1,9 @@
 package com.zzang.chongdae.notification.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.zzang.chongdae.global.service.ServiceTest;
+import com.zzang.chongdae.notification.exception.NotificationRetryableException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,7 @@ public class RetryableTest extends ServiceTest {
     @DisplayName("FCM에서 5xx 에러를 반환할 경우 3번 재시도한다.")
     @Test
     void name() {
-        sender.throwNotificationRetryableException();
+        assertThatThrownBy(() -> sender.throwNotificationRetryableException())
+                .isInstanceOf(NotificationRetryableException.class);
     }
 }
