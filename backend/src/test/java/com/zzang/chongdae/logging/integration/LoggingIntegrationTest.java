2c67c24e5ce43e80f8a1bad7b5633aad9f8c6079
fromitive <fromitive@gmail.com>
2025-04-27 17:38:03 +0900

    test: 테스트 메서드 네이밍 개선

--- backend/src/test/java/com/zzang/chongdae/logging/integration/LoggingIntegrationTest.java
+++ backend/src/test/java/com/zzang/chongdae/logging/integration/LoggingIntegrationTest.java
@@ -43,7 +43,7 @@ public class LoggingIntegrationTest extends IntegrationTest {
 
     @DisplayName("500 오류가 발생할 경우 stackTrace가 기록되어야 한다.")
     @Test
-    void should_loggingStackStrace_when_500_errorOccurred() {
+    void should_log_stackStrace_when_500_errorOccurred() {
         given().log().all()
                 .when().get("/read-only/error/internal")
                 .then().log().all()
@@ -55,7 +55,7 @@ public class LoggingIntegrationTest extends IntegrationTest {
 
     @DisplayName("interceptor 접근권한 오류가 발생해도 로깅이 되어야 한다.")
     @Test
-    void should_logging_when_accessDeniedErrorOccurred() {
+    void should_log_when_accessDeniedErrorOccurred() {
         given().log().all()
                 .when().get("/test")
                 .then().log().all()
@@ -67,7 +67,7 @@ public class LoggingIntegrationTest extends IntegrationTest {
 
     @DisplayName("중요정보는 마스킹되어 로깅해야 한다.")
     @Test
-    void should_maskedLogging_when_annotatedMaskedLogging() {
+    void should_log_masked_requestBody_when_annotatedMaskedLogging() {
         SecretRequest request = new SecretRequest("secret information");
         given().log().all()
                 .contentType(ContentType.JSON)
