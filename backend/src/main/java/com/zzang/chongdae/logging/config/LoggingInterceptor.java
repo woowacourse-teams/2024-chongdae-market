7adfd10b34fbd0e5135d3920ccd2f97fc6d8f60f
MooSong Lee <46563149+fromitive@users.noreply.github.com>
2025-05-05 20:43:29 +0900

    refactor: 로깅 리팩터링 적용 (#738)
    * refactor: CachedHttpServletXXX 제거
    
    * test: logger 테스트 코드 추가
    
    * feat: 요청 내용 마스킹 로깅 기능 구현
    
    * feat: 등록되지 않은 handler 오류 발생 시 로깅하는 기능 구현
    
    * fix: 테스트 오류 수정
    
    * fix: responseBody가 hashCode로만 출력되는 현상 수정
    
    * style: 사소한 컨벤션 수정
    
    * refactor: 가독성 있도록 statement 순서 변경
    
    * test: 로깅의 범위를 app에 한정 짓도록 변경
    
    * refactor: 실제 로깅을 filter에서 하도록 변경
    
    * refactor: 레벨 별 로깅 책임을 HttpStatusCategory가 담당하도록 변경
    
    * test: multipart requestBody 로깅 테스트 추가
    
    * test: 테스트 메서드 네이밍 개선
    
    * refactor: 불필요한 상수 제거
    
    * refactor: 불필요한 객체 제거
    
    * feat: 비동기 쓰레드에 MDC Context 전달하도록 변경
    
    * refactor: mdc LOG_KEY_NAME을 application.yml에서 가져오도록 변경
    
    * refactor: trackId를 전역적으로 위치하도록 변경
    
    * style: eof 제거

--- backend/src/main/java/com/zzang/chongdae/logging/config/LoggingInterceptor.java
+++ backend/src/main/java/com/zzang/chongdae/logging/config/LoggingInterceptor.java
@@ -1,16 +1,9 @@
 package com.zzang.chongdae.logging.config;
 
-import com.zzang.chongdae.global.exception.MarketException;
-import com.zzang.chongdae.logging.domain.HttpStatusCategory;
-import com.zzang.chongdae.logging.domain.MemberIdentifier;
-import com.zzang.chongdae.logging.dto.LoggingInfoFailResponse;
-import com.zzang.chongdae.logging.dto.LoggingInfoSuccessResponse;
-import com.zzang.chongdae.logging.dto.LoggingWarnResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -22,7 +15,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        request.setAttribute("startTime", System.currentTimeMillis());
         return true;
     }
 
@@ -31,80 +23,12 @@ public class LoggingInterceptor implements HandlerInterceptor {
                                 HttpServletResponse response,
                                 Object handler,
                                 Exception ex) throws IOException {
-
         if (!(handler instanceof HandlerMethod handlerMethod)) {
             return;
         }
         Method method = handlerMethod.getMethod();
         if (method.isAnnotationPresent(LoggingMasked.class)) {
-            return;
+            request.setAttribute("loggingMasked", true);
         }
-
-        if (isMultipart(request)) {
-            return;
-        }
-
-        long startTime = Long.parseLong(request.getAttribute("startTime").toString());
-        long endTime = System.currentTimeMillis();
-        String latency = endTime - startTime + "ms";
-
-        String identifier = UUID.randomUUID().toString();
-        MemberIdentifier memberIdentifier = new MemberIdentifier(request.getCookies());
-        String httpMethod = request.getMethod();
-        String uri = parseUri(request.getRequestURI(), request.getQueryString());
-        String requestBody = new String(request.getInputStream().readAllBytes());
-
-        CachedHttpServletResponseWrapper cachedResponse = (CachedHttpServletResponseWrapper) response;
-        String statusCode = String.valueOf(cachedResponse.getStatus());
-        String responseBody = new String(cachedResponse.getCachedBody());
-
-        HttpStatusCategory statusCategory = HttpStatusCategory.fromStatusCode(cachedResponse.getStatus());
-        if (statusCategory == HttpStatusCategory.INFO_SUCCESS) {
-            LoggingInfoSuccessResponse logResponse = new LoggingInfoSuccessResponse(
-                    identifier,
-                    memberIdentifier.getIdInfo(),
-                    httpMethod,
-                    uri,
-                    requestBody,
-                    statusCode,
-                    latency);
-            log.info(logResponse.toString());
-        }
-        if (statusCategory == HttpStatusCategory.INFO_FAIL) {
-            LoggingInfoFailResponse logResponse = new LoggingInfoFailResponse(
-                    identifier,
-                    memberIdentifier.getIdInfo(),
-                    httpMethod,
-                    uri,
-                    requestBody,
-                    statusCode,
-                    responseBody,
-                    latency);
-            log.info(logResponse.toString());
-        }
-        if (statusCategory == HttpStatusCategory.WARN && ex instanceof MarketException) {
-            LoggingWarnResponse logResponse = new LoggingWarnResponse(
-                    identifier,
-                    memberIdentifier.getIdInfo(),
-                    httpMethod,
-                    uri,
-                    requestBody,
-                    statusCode,
-                    responseBody,
-                    latency);
-            log.warn(logResponse.toString());
-        }
-    }
-
-    private boolean isMultipart(HttpServletRequest request) {
-        String contentType = request.getContentType();
-        return contentType != null && contentType.toLowerCase().startsWith("multipart/");
-    }
-
-    private String parseUri(String requestUri, String queryString) {
-        if (queryString == null) {
-            return requestUri;
-        }
-        return requestUri + "?" + queryString;
     }
 }
