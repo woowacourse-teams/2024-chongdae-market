b1f03d7ea166773f92bac6ba3b53d078f0bb2f97
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-05-28 17:42:01 +0900

    feat: 채팅방 usecase 구현

---
+++ android/app/src/main/java/com/zzang/chongdae/domain/usecase/commentdetail/SaveCommentUseCase.kt
@@ -0,0 +1,16 @@
+package com.zzang.chongdae.domain.usecase.commentdetail
+
+import com.zzang.chongdae.di.annotations.CommentDetailRepositoryQualifier
+import com.zzang.chongdae.domain.repository.CommentDetailRepository
+import javax.inject.Inject
+
+class SaveCommentUseCase
+    @Inject
+    constructor(
+        @CommentDetailRepositoryQualifier private val repository: CommentDetailRepository,
+    ) {
+        suspend operator fun invoke(
+            offeringId: Long,
+            content: String,
+        ) = repository.saveComment(offeringId, content)
+    }
