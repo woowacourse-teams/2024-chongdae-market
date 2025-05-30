6e984390e986d81a104e3c26341034700bb3d169
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-05-30 11:30:53 +0900

    feat: 닉네임 변경 api data layer 구현

--- android/app/src/main/java/com/zzang/chongdae/data/remote/source/ParticipantRemoteDataSourceImpl.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/remote/source/ParticipantRemoteDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.zzang.chongdae.data.remote.source
 import com.zzang.chongdae.common.handler.DataError
 import com.zzang.chongdae.common.handler.Result
 import com.zzang.chongdae.data.remote.api.ParticipationApiService
+import com.zzang.chongdae.data.remote.dto.request.NicknameRequest
 import com.zzang.chongdae.data.remote.dto.response.participants.ParticipantsResponse
 import com.zzang.chongdae.data.remote.util.safeApiCall
 import com.zzang.chongdae.data.source.ParticipantRemoteDataSource
@@ -21,5 +22,10 @@ class ParticipantRemoteDataSourceImpl
         override suspend fun deleteParticipations(offeringId: Long): Result<Unit, DataError.Network> =
             safeApiCall {
                 service.deleteParticipations(offeringId)
+            }
+
+        override suspend fun patchNickname(nicknameRequest: NicknameRequest): Result<Unit, DataError.Network> =
+            safeApiCall {
+                service.patchNickname(nicknameRequest)
             }
     }
--- android/app/src/main/java/com/zzang/chongdae/data/source/ParticipantRemoteDataSource.kt
+++ android/app/src/main/java/com/zzang/chongdae/data/source/ParticipantRemoteDataSource.kt
@@ -2,10 +2,13 @@ package com.zzang.chongdae.data.source
 
 import com.zzang.chongdae.common.handler.DataError
 import com.zzang.chongdae.common.handler.Result
+import com.zzang.chongdae.data.remote.dto.request.NicknameRequest
 import com.zzang.chongdae.data.remote.dto.response.participants.ParticipantsResponse
 
 interface ParticipantRemoteDataSource {
     suspend fun fetchParticipants(offeringId: Long): Result<ParticipantsResponse, DataError.Network>
 
     suspend fun deleteParticipations(offeringId: Long): Result<Unit, DataError.Network>
+
+    suspend fun patchNickname(nicknameRequest: NicknameRequest): Result<Unit, DataError.Network>
 }
